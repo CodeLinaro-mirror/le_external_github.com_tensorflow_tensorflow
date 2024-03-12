c6a6601fdc3900adbd76fefacf704ba9a9694b18
Yang Chen <yangchen@google.com>
2024-03-12 11:11:20 -0700

    #tf-data-service Merge duplicate method `GetIteratorContextWithIndexMapper`.
    Also add a simple class to select the right `IteratorContext` when index
    shuffling is needed.
    
    PiperOrigin-RevId: 615107953

--- tensorflow/core/framework/dataset.h
+++ tensorflow/core/framework/dataset.h
@@ -1025,6 +1025,13 @@ class IteratorBase : public Checkpointable {
     return GetNext(&ctx, out_tensors, end_of_sequence);
   }
 
+  // If a dataset needs to provide its own index mapper behavior to support
+  // global shuffling, implement this method.
+  virtual IndexMapperFn GetIndexMapper(
+      IndexMapperFn parent_index_mapper) const {
+    return parent_index_mapper;
+  }
+
   // Skips the next `num_to_skip` outputs from the range that this iterator
   // is traversing.
   //
@@ -1650,6 +1657,53 @@ Status ParseVectorArgument(OpKernelContext* ctx,
   }
   return OkStatus();
 }
+
+// Builds and selects the `IteratorContext` to use based on whether the dataset
+// is globally shuffled.
+//
+// Example usage:
+// In `Iterator::GetNextInternal`:
+//
+//   IteratorContextWithIndexMapper ctx_with_index_mapper(ctx, this);
+//   TF_RETURN_IF_ERROR(input_impl_->GetNext(
+//       ctx_with_index_mapper.Get(), out_tensors, end_of_sequence));
+//   ctx_with_index_mapper.MergeCheckpoint();
+//
+// The iterator also needs to implement `GetIndexMapper` if it should customize
+// the index mapping behavior.
+class IteratorContextWithIndexMapper {
+ public:
+  // Caller keeps ownership of both pointers.
+  explicit IteratorContextWithIndexMapper(IteratorContext* ctx,
+                                          const IteratorBase* iterator)
+      : ctx_(ctx) {
+    if (ctx_->index_mapper()) {
+      IteratorContext::Params params(ctx_);
+      params.index_mapper = iterator->GetIndexMapper(ctx_->index_mapper());
+      ctx_with_index_mapper_.emplace(params);
+    }
+  }
+  virtual ~IteratorContextWithIndexMapper() = default;
+  IteratorContextWithIndexMapper(const IteratorContextWithIndexMapper&) =
+      delete;
+  IteratorContextWithIndexMapper& operator=(
+      const IteratorContextWithIndexMapper&) = delete;
+
+  IteratorContext* Get() {
+    return ctx_with_index_mapper_.has_value() ? &ctx_with_index_mapper_.value()
+                                              : ctx_;
+  }
+
+  void MergeCheckpoint() {
+    if (ctx_with_index_mapper_.has_value()) {
+      ctx_->MergeCheckpoint(ctx_with_index_mapper_->checkpoint());
+    }
+  }
+
+ private:
+  IteratorContext* ctx_;
+  std::optional<IteratorContext> ctx_with_index_mapper_;
+};
 
 // Encapsulates the work required to plug a DatasetBase into the core TensorFlow
 // graph execution engine.
