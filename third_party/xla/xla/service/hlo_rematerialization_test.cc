42fc1bc9c03e14b75a6933a89e2249f5c287497b
Adrian Kuegel <akuegel@google.com>
2024-04-29 02:05:34 -0700

    Allow HloDCE to remove unused tuple elements of MOF
    Multi-Output fusions (MOF) should not have tuple outputs that are unused. This
    change adds logic to clean up such fusions.
    
    PiperOrigin-RevId: 628998676

--- third_party/xla/xla/service/hlo_rematerialization_test.cc
+++ third_party/xla/xla/service/hlo_rematerialization_test.cc
@@ -625,8 +625,7 @@ ENTRY %entry {
                               /*memory_limit_bytes=*/11 * 1024, module.get()));
   EXPECT_TRUE(changed);
   ASSERT_THAT(
-      add, op::Add(op::Multiply(), op::GetTupleElement(AllOf(
-                                       op::Fusion(), ::testing::Ne(fusion)))));
+      add, op::Add(op::Multiply(), AllOf(op::Fusion(), ::testing::Ne(fusion))));
 }
 
 TEST_F(RecomputeAndCompressHloRematerializationTest, RematTupleShapeDoubleUse) {
@@ -725,9 +724,9 @@ ENTRY %entry {
                           RunHloRematerialization(
                               /*memory_limit_bytes=*/11 * 1024, module.get()));
   EXPECT_TRUE(changed);
-  ASSERT_THAT(add, op::Add(op::Bitcast(op::Multiply()),
-                           op::Bitcast(op::GetTupleElement(
-                               AllOf(op::Fusion(), ::testing::Ne(fusion))))));
+  ASSERT_THAT(add,
+              op::Add(op::Bitcast(op::Multiply()),
+                      op::Bitcast(AllOf(op::Fusion(), ::testing::Ne(fusion)))));
 }
 
 TEST_F(RecomputeAndCompressHloRematerializationTest, RematThroughTuple) {
@@ -775,10 +774,9 @@ ENTRY %entry {
                           RunHloRematerialization(
                               /*memory_limit_bytes=*/11 * 1024, module.get()));
   EXPECT_TRUE(changed);
-  ASSERT_THAT(
-      add, op::Add(op::GetTupleElement(AllOf(op::Fusion(), ::testing::Ne(tuple),
-                                             ::testing::Ne(fusion))),
-                   op::Add()));
+  ASSERT_THAT(add, op::Add(AllOf(op::Fusion(), ::testing::Ne(tuple),
+                                 ::testing::Ne(fusion)),
+                           op::Add()));
 }
 
 // Make sure when rematerializing all-gathers we increment channel_ids properly.
