29a1e7749f6ee3f6a9991d41c0630ba2f1297909
Adam Banaś <adambanas@google.com>
2024-06-05 02:36:02 -0700

    [xla:cpu] Add support for convolution thunks
    This implementation has similar performance to the current runtime for 3x3 convolution shapes in `xla/service/cpu/benchmarks/convolution_benchmark_test.cc` benchmark, for both multi-threaded and single-threaded versions. Change doesn't affect 1x1 convolutions in this benchmark, because `convolution` is rewritten as `dot` op by HLO passes in this case.
    
    All features have been ported from the current runtime, only ACL convolutions were not tested.
    
    Added convolution thunk unit tests, they cover some extra cases like F16 convolution and incorrect input data. Note F16 is not actually tested in convolution E2E tests (xla/tests/convolution_test.cc), because F16 is rewritten as F32 convolution there.
    
    Reverts 38dd164bc36e7a2506235ea2561d71da4185bdca
    
    PiperOrigin-RevId: 640454635

--- third_party/xla/xla/service/cpu/thunk_emitter.h
+++ third_party/xla/xla/service/cpu/thunk_emitter.h
@@ -76,6 +76,9 @@ class ThunkEmitter {
   absl::StatusOr<ThunkSequence> EmitConcatenateThunk(
       const HloInstruction* instruction);
 
+  absl::StatusOr<ThunkSequence> EmitConvolutionThunk(
+      const HloInstruction* instruction);
+
   absl::StatusOr<ThunkSequence> EmitCopyThunk(
       const HloInstruction* instruction);
 
