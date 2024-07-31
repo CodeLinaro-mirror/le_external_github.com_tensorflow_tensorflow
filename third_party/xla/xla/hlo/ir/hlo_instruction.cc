5bd4fb4c755318bf00bcd513d4ba118e7d667783
Dimitar (Mitko) Asenov <dasenov@google.com>
2024-07-31 05:49:33 -0700

    [XLA] Sort the backend config when printing `HloInstruction`.
    The canonical HLO instruction text is used in auto-tuning results as part of the key and it includes the backend config as a json string. However, the normal json serialization is non-deterministic, which causes spurious auto-tuning key mismatches. This change fixes the mismatches by enforcing a canonical json ordering.
    
    PiperOrigin-RevId: 657975195

--- third_party/xla/xla/hlo/ir/hlo_instruction.cc
+++ third_party/xla/xla/hlo/ir/hlo_instruction.cc
@@ -75,6 +75,7 @@ limitations under the License.
 #include "xla/service/name_uniquer.h"
 #include "xla/shape.h"
 #include "xla/shape_util.h"
+#include "xla/sort_json.h"
 #include "xla/status_macros.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
@@ -3635,15 +3636,18 @@ void HloInstruction::PrintWithCanonicalNameMap(
   }
   if (options.print_backend_config() && !backend_config_.empty()) {
     absl::string_view config = backend_config_.GetRawString();
+    // Use `value_or` below, because the backend config string isn't guaranteed
+    // to be a JSON string.
+    std::string sorted_config = SortJson(config).value_or(std::string(config));
     printer->Append(", backend_config=");
     // In the common case that the backend-config is valid-ish JSON, the parser
     // doesn't need it delimited by quotes, so we can print it without
     // CEsape'ing.  This is much easier to read.
-    if (LexesAsJsonDict(config)) {
-      printer->Append(config);
+    if (LexesAsJsonDict(sorted_config)) {
+      printer->Append(sorted_config);
     } else {
       printer->Append("\"");
-      printer->Append(CEscape(config));
+      printer->Append(CEscape(sorted_config));
       printer->Append("\"");
     }
   }
