3605891bed2367ef6193c154ad2dfb81b964bbd3
Sandeep Dasgupta <sdasgup@google.com>
2024-09-17 17:35:01 -0700

    [HLO Componentization] Create hlo/translate sub-component. This CL takes care of 1. Migrating xla/translate --> xla/hlo/translate 2. Setting up build aliases in xla/translate ensuring external dependencies are still satisfied.
    PiperOrigin-RevId: 675770530

--- third_party/xla/xla/translate/mhlo_to_hlo/translate_registration.h
+++ third_party/xla/xla/translate/mhlo_to_hlo/translate_registration.h
@@ -16,48 +16,7 @@ limitations under the License.
 #ifndef XLA_TRANSLATE_MHLO_TO_HLO_TRANSLATE_REGISTRATION_H_
 #define XLA_TRANSLATE_MHLO_TO_HLO_TRANSLATE_REGISTRATION_H_
 
-#include "llvm/Support/CommandLine.h"
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> emit_use_tuple_arg(
-    "emit-use-tuple-args",
-    llvm::cl::desc(
-        "Emit HLO modules using tuples as args for the entry computation"),
-    llvm::cl::init(false));
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> emit_return_tuple(
-    "emit-return-tuple",
-    llvm::cl::desc("Emit HLO modules with entry computations returning tuple"),
-    llvm::cl::init(false));
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> with_layouts(
-    "with-layouts",
-    llvm::cl::desc("Propagate layouts when translating MHLO->XLA HLO"),
-    llvm::cl::init(false));
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> print_layouts(
-    "print-layouts", llvm::cl::desc("Print layouts in the generated HLO text"),
-    llvm::cl::init(false));
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> print_large_constants(
-    "print-large-constants",
-    llvm::cl::desc("Print large constants in the generated HLO text"),
-    llvm::cl::init(false));
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> print_sugar(
-    "print-sugar",
-    llvm::cl::desc(
-        "Print async ops using syntactic sugar in the generated HLO text"),
-    llvm::cl::init(true));
-
-// NOLINTNEXTLINE
-llvm::cl::opt<bool> via_builder(
-    "via-builder", llvm::cl::desc("Translate MHLO->XLA HLO via XLA Builder"),
-    llvm::cl::init(false));
+// The current header will be deprecated in favour of the following.
+#include "xla/hlo/translate/mhlo_to_hlo/translate_registration.h"
 
 #endif  // XLA_TRANSLATE_MHLO_TO_HLO_TRANSLATE_REGISTRATION_H_
