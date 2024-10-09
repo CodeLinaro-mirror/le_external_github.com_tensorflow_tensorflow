760ed0827bcc242d534bfbeeddc4532236ebe30b
Henning Becker <hebecker@google.com>
2024-10-08 18:09:51 -0700

    Split GpuEvent into platform specific implementations
    - Removes `GpuEvent` and moves all remaining functionality into `CudaEvent`/`RocmEvent`
    - Moves `DestroyEvent` and `InitEvent` from `GpuDriver` into `CudaEvent` and `RocmEvent`
    - Makes `CudaTimer` and `RocmTimer` use `CudaEvent` and `RocmEvent` as a replacement for `GpuEvent`.
    - Replace `GpuEvent::Init` function by factory functions in `CudaEvent` and `RocmEvent`
    - Add basic test or `CudaEvent` and `RocmEvent`.
    
    PiperOrigin-RevId: 683830727

--- third_party/xla/xla/stream_executor/rocm/rocm_executor.cc
+++ third_party/xla/xla/stream_executor/rocm/rocm_executor.cc
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+
 #include "xla/stream_executor/rocm/rocm_executor.h"
 
 #include <unistd.h>
@@ -52,7 +53,6 @@ limitations under the License.
 #include "xla/stream_executor/gpu/gpu_command_buffer.h"
 #include "xla/stream_executor/gpu/gpu_diagnostics.h"
 #include "xla/stream_executor/gpu/gpu_driver.h"
-#include "xla/stream_executor/gpu/gpu_event.h"
 #include "xla/stream_executor/gpu/gpu_executor.h"
 #include "xla/stream_executor/gpu/gpu_kernel.h"
 #include "xla/stream_executor/gpu/gpu_stream.h"
@@ -648,11 +648,12 @@ absl::Status FillBlockDimLimit(GpuDeviceHandle device,
   return absl::OkStatus();
 }
 
-absl::StatusOr<std::unique_ptr<GpuEvent>> RocmExecutor::CreateGpuEvent(
+absl::StatusOr<std::unique_ptr<RocmEvent>> RocmExecutor::CreateGpuEvent(
     bool allow_timing) {
-  auto gpu_event = std::make_unique<RocmEvent>(gpu_context());
-  TF_RETURN_IF_ERROR(gpu_event->Init(allow_timing));
-  return std::move(gpu_event);
+  TF_ASSIGN_OR_RETURN(
+      auto event,
+      RocmEvent::Create(gpu_context(), /*allow_timing=*/allow_timing));
+  return std::make_unique<RocmEvent>(std::move(event));
 }
 
 absl::StatusOr<std::unique_ptr<Event>> RocmExecutor::CreateEvent() {
@@ -661,7 +662,8 @@ absl::StatusOr<std::unique_ptr<Event>> RocmExecutor::CreateEvent() {
 
 absl::StatusOr<std::unique_ptr<Stream>> RocmExecutor::CreateStream(
     std::optional<std::variant<StreamPriority, int>> priority) {
-  TF_ASSIGN_OR_RETURN(auto event, CreateGpuEvent(/*allow_timing=*/false));
+  TF_ASSIGN_OR_RETURN(auto event,
+                      RocmEvent::Create(gpu_context(), /*allow_timing=*/false));
   TF_ASSIGN_OR_RETURN(auto stream,
                       RocmStream::Create(this, std::move(event), priority));
   absl::MutexLock l(&alive_gpu_streams_mu_);
