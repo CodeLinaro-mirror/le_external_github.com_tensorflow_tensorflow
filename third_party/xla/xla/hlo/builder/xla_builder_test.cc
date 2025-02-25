andypeng2015/xla

[XLA] {in,out}feed SPMD spring cleaning
A pletora of changes:
1. Correct sharding generated for the infeed token in XlaBuilder.
2. Make SpmdPartitioner not pad infeed with manual sharding.
3. Make {in,out}feed rewrite passes work with sharding.

PiperOrigin-RevId: 724473186

xla/hlo/builder/xla_builder_test.cc
@@ -3677,5 +3677,25 @@ TEST(XlaBuilderTest, UnorderedOutfeed) {
                                   m::Outfeed(m::Parameter(0), m::AfterAll()))));
 }
 
+TEST(XlaBuilderTest, InfeedTokenSharding) {
+  XlaBuilder b(TestName());
+  b.SetSharding(sharding_builder::Replicate());
+  const Shape s = ShapeUtil::MakeShape(PRED, {});
+  XlaOp infeed0 = Infeed(&b, s);
+  XlaOp infeed1 = Infeed(&b, s);
+  And(infeed0, infeed1);
+  TF_ASSERT_OK_AND_ASSIGN(const auto module, BuildHloModule(b));
+  TF_ASSERT_OK_AND_ASSIGN(
+      const auto token_sharding,
+      HloSharding::FromProto(sharding_builder::AssignDevice(0)));
+  for (const HloInstruction* instruction :
+       module->entry_computation()->instructions()) {
+    if (instruction->shape().IsToken()) {
+      EXPECT_TRUE(instruction->has_sharding());
+      EXPECT_EQ(instruction->sharding(), token_sharding);
+    }
+  }
+}
+
 }  // namespace
 }  // namespace xla

CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

third_party/xla/xla/hlo/builder/xla_builder_test.cc
@@ -3702,5 +3702,25 @@ TEST(XlaBuilderTest, UnorderedOutfeed) {
                                   m::Outfeed(m::Parameter(0), m::AfterAll()))));
 }
 
+TEST(XlaBuilderTest, InfeedTokenSharding) {
+  XlaBuilder b(TestName());
+  b.SetSharding(sharding_builder::Replicate());
+  const Shape s = ShapeUtil::MakeShape(PRED, {});
+  XlaOp infeed0 = Infeed(&b, s);
+  XlaOp infeed1 = Infeed(&b, s);
+  And(infeed0, infeed1);
+  TF_ASSERT_OK_AND_ASSIGN(const auto module, BuildHloModule(b));
+  TF_ASSERT_OK_AND_ASSIGN(
+      const auto token_sharding,
+      HloSharding::FromProto(sharding_builder::AssignDevice(0)));
+  for (const HloInstruction* instruction :
+       module->entry_computation()->instructions()) {
+    if (instruction->shape().IsToken()) {
+      ASSERT_TRUE(instruction->has_sharding());
+      EXPECT_EQ(instruction->sharding(), token_sharding);
+    }
+  }
+}
+
 }  // namespace
 }  // namespace xla