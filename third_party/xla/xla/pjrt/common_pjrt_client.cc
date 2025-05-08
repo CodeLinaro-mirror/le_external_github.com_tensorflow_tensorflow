0238482d9ef1912d97f2ab7d852e7ebf1ef78fe0
Parker Schuh <parkers@google.com>
2025-05-08 12:27:43 -0700

    Add CommonPjRtClient::CreateUninitializedBuffer and remove CpuClient::CreateUninitializedBuffer.
    PiperOrigin-RevId: 756414421

--- third_party/xla/xla/pjrt/common_pjrt_client.cc
+++ third_party/xla/xla/pjrt/common_pjrt_client.cc
@@ -92,6 +92,29 @@ CommonPjRtClient::BufferFromHostLiteral(const LiteralSlice& literal,
   return output_buffer;
 }
 
+absl::StatusOr<std::unique_ptr<PjRtBuffer>>
+CommonPjRtClient::CreateUninitializedBuffer(const Shape& shape,
+                                            PjRtMemorySpace* memory_space) {
+  if (shape.IsTuple()) {
+    return InvalidArgument(
+        "Tuples are not supported in "
+        "CommonPjRtClient::CreateUninitializedBuffer");
+  }
+  TF_ASSIGN_OR_RETURN(Shape device_shape, MakeDefaultShapeForMemorySpace(
+                                              memory_space, shape, nullptr));
+  TF_ASSIGN_OR_RETURN(int64_t on_device_bytes_count,
+                      GetOnDeviceBytesCount(memory_space, device_shape));
+  TF_ASSIGN_OR_RETURN(auto raw_buffer,
+                      AllocateRawBuffer(memory_space, on_device_bytes_count,
+                                        /*allocate_after=*/{}));
+  TF_ASSIGN_OR_RETURN(auto definition_event,
+                      raw_buffer->MakeAllocationReadyEvent());
+  TF_ASSIGN_OR_RETURN(
+      auto output_buffer,
+      DefineBuffer(device_shape, raw_buffer, {std::move(definition_event)}));
+  return output_buffer;
+}
+
 absl::StatusOr<xla::Shape> CommonPjRtClient::MakeDefaultShapeForMemorySpace(
     PjRtMemorySpace* memory_space, xla::Shape shape,
     const xla::Layout* layout) const {
