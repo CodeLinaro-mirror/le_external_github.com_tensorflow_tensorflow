8daa5d54c9dd89c04e5198d430780aff32cf0733
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-15 11:56:29 -0700

    [XLA:CPU] Add xla.libm with ldexp replacements.
    PiperOrigin-RevId: 759240983

--- third_party/xla/xla/backends/cpu/codegen/ir_compiler.cc
+++ third_party/xla/xla/backends/cpu/codegen/ir_compiler.cc
@@ -61,6 +61,7 @@ limitations under the License.
 #include "llvm/Transforms/Instrumentation/DataFlowSanitizer.h"
 #include "xla/backends/cpu/codegen/cpu_features.h"
 #include "xla/backends/cpu/codegen/polynomial_approximations.h"
+#include "xla/codegen/math_approximations_lib.h"
 #include "xla/service/cpu/cpu_options.h"
 #include "xla/service/hlo_module_config.h"
 #include "xla/service/llvm_ir/llvm_util.h"
@@ -334,6 +335,8 @@ llvm::Error IrCompiler::RunIrPasses(llvm::Module& module,
       std::make_unique<llvm::TargetLibraryInfoImpl>(target_triple);
   target_library_info_impl->addVectorizableFunctions(
       PolynomialApproximationsVectorization());
+  codegen::MathFunctionLib math_lib;
+  target_library_info_impl->addVectorizableFunctions(math_lib.Vectorizations());
 
   fam.registerPass(
       [&] { return llvm::TargetLibraryAnalysis(*target_library_info_impl); });
@@ -368,7 +371,9 @@ llvm::Error IrCompiler::RunIrPasses(llvm::Module& module,
     }
   }
 
+  math_lib.BeforeOptimization(module);
   pm.run(module, mam);
+  math_lib.AfterOptimization(module, opt_level);
 
   {
     std::string error_string;
@@ -376,8 +381,8 @@ llvm::Error IrCompiler::RunIrPasses(llvm::Module& module,
     if (llvm::verifyModule(module, &error_stream)) {
       return llvm::make_error<llvm::StringError>(
           llvm::errc::invalid_argument,
-          absl::StrFormat("Invalid LLVM IR after optimizations:\n%s",
-                          error_stream.str()));
+          absl::StrFormat("Invalid LLVM IR after optimizations:\n%s\n%s",
+                          error_stream.str(), llvm_ir::DumpToString(&module)));
     }
   }
 
