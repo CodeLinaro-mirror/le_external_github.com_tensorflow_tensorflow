eb71c024fe90dfddc44e444ef93b9b2d9b85d5c0
Quentin Khan <qkhan@google.com>
2025-06-06 13:00:57 -0700

    Rename `options` to `options_ptr` in `tflite::xnnpack::Delegate::Delegate()`.
    We have been burnt several times by `options` (input parameter pointer) and
    `options_` (delegate instance field) having very close names. This change moves
    the initialization of `options_` at the beginning of the constructor, renames
    `options` to `options_ptr` and replaces all its uses with `options_`.
    
    This will hopefully help avoid future issues.
    
    PiperOrigin-RevId: 768186622

--- tensorflow/lite/delegates/xnnpack/xnnpack_delegate.cc
+++ tensorflow/lite/delegates/xnnpack/xnnpack_delegate.cc
@@ -571,9 +571,10 @@ class Delegate {
   friend class Subgraph;
 
  public:
-  explicit Delegate(const TfLiteXNNPackDelegateOptions* options,
-                    xnn_workspace_t workspace,
-                    TfLiteContext* context = nullptr) {
+  explicit Delegate(const TfLiteXNNPackDelegateOptions* options_ptr,
+                    xnn_workspace_t workspace, TfLiteContext* context = nullptr)
+      : options_(options_ptr ? *options_ptr
+                             : TfLiteXNNPackDelegateOptionsDefault()) {
     int num_subgraphs = 1;
     if (context) {
       tflite::Subgraph* this_subgraph =
@@ -599,9 +600,9 @@ class Delegate {
       own_threadpool_ = false;
     } else {
       own_threadpool_ = true;
-      if (options != nullptr && options->num_threads > 1) {
+      if (options_.num_threads > 1) {
         threadpool_.reset(
-            pthreadpool_create(static_cast<size_t>(options->num_threads)));
+            pthreadpool_create(static_cast<size_t>(options_.num_threads)));
         threadpool = threadpool_.get();
       }
     }
@@ -610,8 +611,6 @@ class Delegate {
     TFLITE_LOG_PROD_ONCE(tflite::TFLITE_LOG_INFO,
                          "Created TensorFlow Lite XNNPACK delegate for CPU.");
 
-    options_ =
-        options != nullptr ? *options : TfLiteXNNPackDelegateOptionsDefault();
     delegate_.flags = GetXNNPackDelegateFlags();
     workspace_.reset(workspace);
 
@@ -1053,8 +1052,8 @@ class Subgraph {
     for (int t : tensors) {
       const TfLiteTensor* tensor = &context->tensors[t];
 
+      const void* data = nullptr;
       uint32_t flags = 0;
-      bool is_external = false;
       if (tensor->type == kTfLiteResource) {
         // We should never see a resource tensor if we are not handling variable
         // ops.
@@ -1075,25 +1074,8 @@ class Subgraph {
         flags = resource->GetValueFlags();
         if (flags == 0) continue;
         tensor = &context->tensors[resource->GetProxyValue()];
-        is_external = true;
         externals.insert(t);
-        if (flags & XNN_VALUE_FLAG_EXTERNAL_OUTPUT) {
-          // Treat this as an output so it gets reshaped?
-          external_outputs.push_back(t);
-        }
-      }
-
-      const xnn_datatype datatype = GetXNNPackDatatype(context, *tensor, t);
-      if (datatype == xnn_datatype_invalid) {
-        TF_LITE_KERNEL_LOG(
-            context,
-            "unsupported datatype (%s) of tensor %d in XNNPACK delegate",
-            TfLiteTypeGetName(tensor->type), t);
-        return nullptr;
-      }
-
-      const void* data = nullptr;
-      if (!is_external) {
+      } else {
         if (tensor->allocation_type == kTfLiteMmapRo) {
           data = tensor->data.raw_const;
         } else {
@@ -1103,19 +1085,18 @@ class Subgraph {
             data = it->second.data();
           }
         }
-      }
-      if (inputs.count(t) != 0) {
-        flags |= XNN_VALUE_FLAG_EXTERNAL_INPUT;
-        if (data == nullptr) {
-          externals.insert(t);
-          external_inputs.push_back(t);
+        if (inputs.count(t) != 0) {
+          flags |= XNN_VALUE_FLAG_EXTERNAL_INPUT;
+          if (data == nullptr) {
+            externals.insert(t);
+            external_inputs.push_back(t);
+          }
+        }
+        if (outputs.count(t) != 0) {
+          flags |= XNN_VALUE_FLAG_EXTERNAL_OUTPUT;
+          external_outputs.push_back(t);
         }
       }
-      if (outputs.count(t) != 0) {
-        flags |= XNN_VALUE_FLAG_EXTERNAL_OUTPUT;
-        external_outputs.push_back(t);
-      }
-
       uint32_t xnnpack_id = XNN_INVALID_VALUE_ID;
       if (DefineXNNPACKValue(context, subgraph.get(), *tensor, t, data, flags,
                              &xnnpack_id) != kTfLiteOk) {
