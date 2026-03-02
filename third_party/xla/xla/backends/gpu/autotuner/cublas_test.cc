7c8e887e6e6bc08da531bd19c74e4e1485357646
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-02 05:00:15 -0800

    Extract cublas(lt) along with get-tuple during autotuning.
    - This is required to support tuple comparision in Autotuner correctness checking, if we enable that without handling cuBLAS(lt), all the cuBLAS configs are rejected from autotuner due to mismatch in the scratch space buffer.
    
    PiperOrigin-RevId: 877341278

--- third_party/xla/xla/backends/gpu/autotuner/cublas_test.cc
+++ third_party/xla/xla/backends/gpu/autotuner/cublas_test.cc
@@ -31,6 +31,7 @@ limitations under the License.
 #include "xla/service/executable.h"
 #include "xla/service/gpu/nvptx_compiler.h"
 #include "xla/service/platform_util.h"
+#include "xla/shape.h"
 #include "xla/stream_executor/blas.h"
 #include "xla/stream_executor/device_description.pb.h"
 #include "xla/stream_executor/stream_executor.h"
@@ -233,8 +234,12 @@ TEST_F(CublasBackendTest, Compile) {
       backend_.GetDefaultConfig(
           *(module->entry_computation()->root_instruction()->operand(0))));
   absl::StatusOr<std::unique_ptr<Executable>> executable = backend_.Compile(
-      *(module->entry_computation()->root_instruction()), *config);
+      *(module->entry_computation()->root_instruction()->operand(0)), *config);
   EXPECT_THAT(executable, IsOk());
+  const ProgramShape& program_shape =
+      (*executable)->compute_computation_layout().ComputeProgramShape();
+  EXPECT_EQ(program_shape.parameters_size(), 2);
+  EXPECT_FALSE(program_shape.result().IsTuple());
 }
 
 }  // namespace gpu
