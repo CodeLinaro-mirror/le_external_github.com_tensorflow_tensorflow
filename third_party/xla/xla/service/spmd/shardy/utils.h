b07ad9ff3ecb65e8786067c56922d4ebe7d2cb7a
Enver Kayaaslan <enver@google.com>
2026-03-25 06:56:28 -0700

    Resolve conflicts by adding reshards also when outsharding of the named computation is empty.
    An empty named computation implies fully replicated shardings and in fact the conflict resolution for the users of the named computation assumes that the sharding is fully replicated and goes ahead with adding reshards on their end. [1, 2, 3]
    
    PiperOrigin-RevId: 889218809

--- third_party/xla/xla/service/spmd/shardy/utils.h
+++ third_party/xla/xla/service/spmd/shardy/utils.h
@@ -161,6 +161,11 @@ bool hasGspmdAttrsOrOps(mlir::ModuleOp module);
 // TODO(b/420837831): delete this once we don't fall back to GSPMD.
 bool hasShardyMesh(mlir::ModuleOp module);
 
+// Returns `TensorShardingPerValueAttr` that is fully closed and like the given
+// `shardings`. Assumes `shardings` is non-empty.
+mlir::sdy::TensorShardingPerValueAttr getFullyClosedLike(
+    mlir::sdy::TensorShardingPerValueAttr shardings);
+
 // Returns the shardings for the results of `funcOp`, with fully replicated
 // shardings for empty shardings on `funcOp`.
 mlir::sdy::TensorShardingPerValueAttr getFuncResultShardings(
@@ -211,20 +216,24 @@ mlir::func::FuncOp cloneFuncRecursively(
 
 // Adds reshard/copy operations to resolve conflicts between call argument
 // sharding and func input sharding. Does not insert reshards in case `funcOp`
-// does not have a non-empty `TensorShardingPerValueAttr` for its arguments.
+// does not have a non-empty `TensorShardingPerValueAttr` for its arguments. The
+// copy operations inserted also have manual axes if `callOp` and `funcOp` do
+// have one. Assumes `callOp` and `funcOp` has identical manual axes or the lack
+// thereof.
 void maybeInsertReshardsOnFuncArguments(mlir::func::FuncOp funcOp,
                                         mlir::func::CallOp callOp,
                                         const mlir::SymbolTable& symbolTable,
                                         mlir::IRRewriter& rewriter);
 
 // Adds reshard/copy operations to resolve conflicts between call result
-// sharding and func output sharding. Sets the call result sharding to the func
-// output shardings. Does not insert reshards in case `funcOp` does not have a
-// non-empty `TensorShardingPerValueAttr` for its results.
-void maybeInsertReshardsOnFuncResults(mlir::func::FuncOp funcOp,
-                                      mlir::func::CallOp callOp,
-                                      const mlir::SymbolTable& symbolTable,
-                                      mlir::IRRewriter& rewriter);
+// sharding and func result sharding. Sets the call result sharding to the func
+// result shardings. The copy operations inserted also have manual axes if
+// `callOp` and `funcOp` do have one. Assumes `callOp` and `funcOp` has
+// identical manual axes or the lack thereof. Assumes `funcResultShardings` is
+// non-empty.
+void insertReshardsOnFuncResults(
+    mlir::sdy::TensorShardingPerValueAttr funcResultShardings,
+    mlir::func::CallOp callOp, mlir::IRRewriter& rewriter);
 
 }  // namespace sdy
 }  // namespace xla
