KauckyAles/tensorflow

[XLA:MSA] Reuse the existing HloAliasAnalysis in CostAnalysis.
PiperOrigin-RevId: 911860008

third_party/xla/xla/service/memory_space_assignment/cost_analysis.h
@@ -97,7 +97,8 @@ class CostAnalysis {
 
   static absl::StatusOr<std::unique_ptr<CostAnalysis>> Create(
       OpCostManager& op_cost_manager, const CostAnalysisOptions& options,
-      const AliasInfo* alias_info, const HloModule& module);
+      const AliasInfo* alias_info, const HloModule& module,
+      HloAliasAnalysis* alias_analysis);
 
   int64_t GetShapeSizeBytes(const Shape& shape) const;
 
@@ -250,11 +251,23 @@ class CostAnalysis {
         hlo_live_range_(std::move(hlo_live_range)),
         call_graph_(std::move(call_graph)) {}
 
+  CostAnalysis(OpCostManager& op_cost_manager,
+               const CostAnalysisOptions& options,
+               HloAliasAnalysis* alias_analysis,
+               std::unique_ptr<HloLiveRange> hlo_live_range,
+               std::unique_ptr<CallGraph> call_graph)
+      : op_cost_manager_(op_cost_manager),
+        options_(options),
+        alias_analysis_ptr_(alias_analysis),
+        hlo_live_range_(std::move(hlo_live_range)),
+        call_graph_(std::move(call_graph)) {}
+
  private:
   // A manager responsible for return basic cost metrics.
   OpCostManager& op_cost_manager_;
   const CostAnalysisOptions options_;
   std::unique_ptr<HloAliasAnalysis> alias_analysis_;
+  HloAliasAnalysis* alias_analysis_ptr_;
   std::unique_ptr<HloLiveRange> hlo_live_range_;
   std::unique_ptr<CallGraph> call_graph_;
 };

CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

third_party/xla/xla/service/memory_space_assignment/cost_analysis.h
@@ -97,7 +97,8 @@ class CostAnalysis {
 
   static absl::StatusOr<std::unique_ptr<CostAnalysis>> Create(
       OpCostManager& op_cost_manager, const CostAnalysisOptions& options,
-      const AliasInfo* alias_info, const HloModule& module);
+      const AliasInfo* alias_info, const HloModule& module,
+      HloAliasAnalysis* alias_analysis);
 
   int64_t GetShapeSizeBytes(const Shape& shape) const;
 
@@ -241,20 +242,15 @@ class CostAnalysis {
  protected:
   CostAnalysis(OpCostManager& op_cost_manager,
                const CostAnalysisOptions& options,
-               std::unique_ptr<HloAliasAnalysis> alias_analysis,
+               HloAliasAnalysis* alias_analysis,
                std::unique_ptr<HloLiveRange> hlo_live_range,
-               std::unique_ptr<CallGraph> call_graph)
-      : op_cost_manager_(op_cost_manager),
-        options_(options),
-        alias_analysis_(std::move(alias_analysis)),
-        hlo_live_range_(std::move(hlo_live_range)),
-        call_graph_(std::move(call_graph)) {}
+               std::unique_ptr<CallGraph> call_graph);
 
  private:
   // A manager responsible for return basic cost metrics.
   OpCostManager& op_cost_manager_;
   const CostAnalysisOptions options_;
-  std::unique_ptr<HloAliasAnalysis> alias_analysis_;
+  HloAliasAnalysis* alias_analysis_;
   std::unique_ptr<HloLiveRange> hlo_live_range_;
   std::unique_ptr<CallGraph> call_graph_;
 };