Erims-Svcbtc/tensorflow

[XLA:CPU] CPU Runtime & Thunk Emitter Support for RngSeedThunk.
PiperOrigin-RevId: 914745007

tensorflow/compiler/aot/thunk_proto_execution_deserializer.cc
@@ -118,6 +118,11 @@ ThunkProtoExecutionDeserializer::ThunkSpecificRunImplFromThunkSequence(
                             GetTopKThunkRunImpl(thunk));
         break;
       }
+      case xla::cpu::ThunkProto::kRngSeedThunk: {
+        TF_ASSIGN_OR_RETURN(thunk_run_impls.emplace_back(),
+                            GetRngSeedThunkRunImpl(thunk));
+        break;
+      }
       default: {
         return xla::Internal("Unsupported thunk type: %s.", thunk.kind());
       }
@@ -428,6 +433,36 @@ ThunkProtoExecutionDeserializer::GetRngGetAndUpdateStateThunkRunImpl(
        {"{{RNG_STATE_PTR}}",
         absl::StrCat("reinterpret_cast<uint64_t*>(",
                      GetBufferAllocationString(rng_thunk.state_buffer()),
+                     ")")}});
+}
+
+absl::StatusOr<std::string>
+ThunkProtoExecutionDeserializer::GetRngSeedThunkRunImpl(
+    const xla::cpu::ThunkProto& thunk) {
+  if (!thunk.has_rng_seed_thunk()) {
+    return xla::Internal(
+        "RngSeed thunk was expected when getting thunk run implementation.");
+  }
+  const xla::cpu::RngSeedThunkProto& rng_seed_thunk = thunk.rng_seed_thunk();
+
+  absl::string_view rng_seed_thunk_invocation_format = R"(
+     // Rng Seed Thunk
+     {
+         uint64_t seed = static_cast<uint32_t>(run_options->rng_seed());
+         if (seed == 0) {
+           static thread_local std::random_device rd;
+           static thread_local std::mt19937_64 gen(rd());
+           std::uniform_int_distribution<uint64_t> distrib(1, std::numeric_limits<uint64_t>::max());
+           seed = distrib(gen);
+         }
+         *reinterpret_cast<uint64_t*>({{RNG_SEED_PTR}}) = seed;
+     })";
+
+  return absl::StrReplaceAll(
+      rng_seed_thunk_invocation_format,
+      {{"{{RNG_SEED_PTR}}",
+        absl::StrCat("reinterpret_cast<uint64_t*>(",
+                     GetBufferAllocationString(rng_seed_thunk.dest_buffer()),
                      ")")}});
 }
 

third_party/xla/xla/backends/cpu/runtime/rng_seed_thunk.cc
@@ -1,0 +1,86 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
+
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <utility>
+
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_format.h"
+#include "xla/backends/cpu/runtime/thunk.h"
+#include "xla/runtime/buffer_use.h"
+#include "xla/service/buffer_assignment.h"
+#include "xla/shape_util.h"
+#include "xla/stream_executor/device_address.h"
+#include "xla/tsl/concurrency/async_value_ref.h"
+#include "xla/util.h"
+#include "tsl/platform/random.h"
+
+namespace xla::cpu {
+
+absl::StatusOr<std::unique_ptr<RngSeedThunk>> RngSeedThunk::Create(
+    Info info, BufferAllocation::Slice dest_buffer) {
+  return std::make_unique<RngSeedThunk>(std::move(info), dest_buffer);
+}
+
+RngSeedThunk::RngSeedThunk(Info info, BufferAllocation::Slice dest_buffer)
+    : Thunk(Kind::kRngSeed, std::move(info)), dest_buffer_(dest_buffer) {}
+
+tsl::AsyncValueRef<Thunk::ExecuteEvent> RngSeedThunk::Execute(
+    const ExecuteParams& params) {
+  if (params.buffer_allocations == nullptr) {
+    return InvalidArgument("buffer_allocations cannot be null");
+  }
+  auto status_or_addr =
+      params.buffer_allocations->GetDeviceAddress(dest_buffer_);
+  if (!status_or_addr.ok()) {
+    return tsl::MakeErrorAsyncValueRef(status_or_addr.status());
+  }
+  se::DeviceAddressBase dest_data = *status_or_addr;
+  if (dest_data.opaque() == nullptr) {
+    return InvalidArgument("seed buffer opaque address cannot be null");
+  }
+
+  if (dest_data.size() != sizeof(uint64_t)) {
+    return InvalidArgument("Invalid seed buffer size: %u", dest_data.size());
+  }
+
+  uint64_t seed = static_cast<uint32_t>(params.rng_seed);
+  if (seed == 0) {
+    // 0 is avoided because it is used as a sentinel value indicating that a
+    // random seed should be generated.
+    do {
+      seed = tsl::random::New64();
+    } while (seed == 0);
+  }
+
+  VLOG(3) << absl::StreamFormat("RngSeedThunk: seed=%d", seed);
+  VLOG(3) << absl::StreamFormat("  dest: %s (%p)", dest_buffer_.ToString(),
+                                dest_data.opaque());
+
+  std::memcpy(dest_data.opaque(), &seed, sizeof(uint64_t));
+
+  return OkExecuteEvent();
+}
+
+Thunk::BufferUses RngSeedThunk::buffer_uses() const {
+  return {BufferUse::Write(dest_buffer_, ShapeUtil::MakeShape(U64, {}))};
+}
+
+}  // namespace xla::cpu

third_party/xla/xla/backends/cpu/runtime/thunk_proto_serdes.cc
@@ -25,6 +25,7 @@ limitations under the License.
 #include <vector>
 
 #include "absl/algorithm/container.h"
+#include "absl/base/casts.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/check.h"
@@ -40,6 +41,7 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/kernel_thunk.h"
 #include "xla/backends/cpu/runtime/logical_id_thunk.h"
 #include "xla/backends/cpu/runtime/outfeed_thunk.h"
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
 #include "xla/backends/cpu/runtime/rng_state_thunk.h"
 #include "xla/backends/cpu/runtime/serdes_base.h"
 #include "xla/backends/cpu/runtime/sort_thunk.h"
@@ -167,6 +169,8 @@ static absl::StatusOr<Thunk::Kind> ProtoThunkToThunkKind(
       return Thunk::Kind::kReplicaId;
     case ThunkProto::ImplCase::kYnnFusionThunk:
       return Thunk::Kind::kYnnFusion;
+    case ThunkProto::ImplCase::kRngSeedThunk:
+      return Thunk::Kind::kRngSeed;
     case ThunkProto::ImplCase::IMPL_NOT_SET:
       return Internal("Thunk kind not set.");
   }
@@ -445,6 +449,12 @@ static absl::Status ToProto(const ReplicaIdThunk& thunk, ThunkProto& proto) {
   return absl::OkStatus();
 }
 
+static absl::Status ToProto(const RngSeedThunk& thunk, ThunkProto& proto) {
+  TF_ASSIGN_OR_RETURN(*proto.mutable_rng_seed_thunk()->mutable_dest_buffer(),
+                      thunk.dest_buffer().ToProto());
+  return absl::OkStatus();
+}
+
 absl::StatusOr<ThunkProto> ThunkSerDesProtobuf::ToProto(
     const Thunk& thunk) const {
   ThunkProto proto;
@@ -511,6 +521,10 @@ absl::StatusOr<ThunkProto> ThunkSerDesProtobuf::ToProto(
               absl::down_cast<const internal::LogicalIdThunk<
                   internal::LogicalIdKind::kReplicaId>&>(thunk)),
           proto));
+      break;
+    case Thunk::Kind::kRngSeed:
+      TF_RETURN_IF_ERROR(::xla::cpu::ToProto(
+          absl::down_cast<const RngSeedThunk&>(thunk), proto));
       break;
     default:
       return absl::UnimplementedError(
@@ -825,6 +839,19 @@ static absl::StatusOr<std::unique_ptr<Thunk>> ReplicaIdThunkFromProto(
       std::move(info), std::move(logical_id_buffer));
 }
 
+static absl::StatusOr<std::unique_ptr<RngSeedThunk>> RngSeedThunkFromProto(
+    const ThunkProto& proto,
+    const std::vector<BufferAllocation>& buffer_allocations) {
+  TF_ASSIGN_OR_RETURN(Thunk::Info info, ThunkInfoFromProto(proto.info()));
+
+  TF_ASSIGN_OR_RETURN(
+      BufferAllocation::Slice dest_buffer,
+      BufferAllocation::Slice::FromProto(proto.rng_seed_thunk().dest_buffer(),
+                                         buffer_allocations));
+
+  return RngSeedThunk::Create(std::move(info), std::move(dest_buffer));
+}
+
 absl::StatusOr<std::unique_ptr<Thunk>> ThunkSerDesProtobuf::FromProto(
     const ThunkProto& proto) const {
   CHECK(buffer_allocations_ != nullptr);
@@ -867,6 +894,8 @@ absl::StatusOr<std::unique_ptr<Thunk>> ThunkSerDesProtobuf::FromProto(
       return PartitionIdThunkFromProto(proto, *buffer_allocations_);
     case Thunk::Kind::kReplicaId:
       return ReplicaIdThunkFromProto(proto, *buffer_allocations_);
+    case Thunk::Kind::kRngSeed:
+      return RngSeedThunkFromProto(proto, *buffer_allocations_);
     default:
       return absl::Status(absl::StatusCode::kInvalidArgument,
                           absl::StrFormat("Unsupported thunk kind: %s",

third_party/xla/xla/backends/cpu/runtime/thunk_sequence_serdes_test.cc
@@ -24,6 +24,7 @@ limitations under the License.
 
 #include <gtest/gtest.h>
 #include "absl/algorithm/container.h"
+#include "absl/base/casts.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/check.h"
@@ -49,6 +50,7 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/logical_id_thunk.h"
 #include "xla/backends/cpu/runtime/outfeed_thunk.h"
 #include "xla/backends/cpu/runtime/reduce_scatter_thunk.h"
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
 #include "xla/backends/cpu/runtime/rng_state_thunk.h"
 #include "xla/backends/cpu/runtime/serdes_base.h"
 #include "xla/backends/cpu/runtime/sort_thunk.h"
@@ -184,6 +186,7 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
     TF_ASSIGN_OR_RETURN(thunk_sequence.emplace_back(),
                         CreateConvolutionThunk());
     TF_ASSIGN_OR_RETURN(thunk_sequence.emplace_back(), CreateSortThunk());
+    TF_ASSIGN_OR_RETURN(thunk_sequence.emplace_back(), CreateRngSeedThunk());
     return thunk_sequence;
   }
 
@@ -681,6 +684,14 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
         /*direction=*/SortThunk::SortDirection::kAscending);
   }
 
+  absl::StatusOr<std::unique_ptr<Thunk>> CreateRngSeedThunk() {
+    TF_RETURN_IF_ERROR(AddBufferAllocations(1));
+    return RngSeedThunk::Create(
+        Thunk::Info(),
+        CreateBufferAllocationSlice(
+            buffer_allocations_[buffer_allocations_.size() - 1]));
+  }
+
   bool VerifySliceEquality(const BufferAllocation::Slice& slice_1,
                            const BufferAllocation::Slice& slice_2) {
     return slice_1.offset() == slice_2.offset() &&
@@ -983,6 +994,11 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
            thunk_1.delta() == thunk_2.delta();
   }
 
+  bool VerifyRngSeedThunkEquality(const RngSeedThunk& thunk_1,
+                                  const RngSeedThunk& thunk_2) {
+    return VerifySliceEquality(thunk_1.dest_buffer(), thunk_2.dest_buffer());
+  }
+
   bool VerifySortThunkEquality(const SortThunk& thunk_1,
                                const SortThunk& thunk_2) {
     return thunk_1.comparator_name() == thunk_2.comparator_name() &&
@@ -1218,6 +1234,10 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
         return VerifyRngGetAndUpdateStateThunkEquality(
             absl::down_cast<const RngGetAndUpdateStateThunk&>(thunk_1),
             absl::down_cast<const RngGetAndUpdateStateThunk&>(thunk_2));
+      case Thunk::Kind::kRngSeed:
+        return VerifyRngSeedThunkEquality(
+            absl::down_cast<const RngSeedThunk&>(thunk_1),
+            absl::down_cast<const RngSeedThunk&>(thunk_2));
       case Thunk::Kind::kSort:
         return VerifySortThunkEquality(
             absl::down_cast<const SortThunk&>(thunk_1),

third_party/xla/xla/service/cpu/thunk_emitter.cc
@@ -64,6 +64,7 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/logical_id_thunk.h"
 #include "xla/backends/cpu/runtime/outfeed_thunk.h"
 #include "xla/backends/cpu/runtime/reduce_scatter_thunk.h"
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
 #include "xla/backends/cpu/runtime/rng_state_thunk.h"
 #include "xla/backends/cpu/runtime/sort_thunk.h"
 #include "xla/backends/cpu/runtime/thunk.h"
@@ -891,6 +892,12 @@ absl::StatusOr<ThunkSequence> ThunkEmitter::EmitRngGetAndUpdateStateThunk(
       ThunkInfo(instruction), state_buffer, rng_state->delta());
 }
 
+absl::StatusOr<ThunkSequence> ThunkEmitter::EmitRngSeedThunk(
+    const HloInstruction* instruction) {
+  TF_ASSIGN_OR_RETURN(auto seed_buffer, GetAllocationSlice(instruction));
+  return ThunkSequence::Of<RngSeedThunk>(ThunkInfo(instruction), seed_buffer);
+}
+
 absl::StatusOr<ThunkSequence> ThunkEmitter::EmitStochasticConvertThunk(
     const HloInstruction* instruction) {
   return Unimplemented("StochasticConvert should be decomposed for CPU.");
@@ -1214,6 +1221,8 @@ absl::StatusOr<ThunkSequence> ThunkEmitter::EmitCustomCallThunk(
     return EmitTopKThunk(custom_call);
   } else if (custom_call_target == "SliceToDynamic") {
     return EmitSliceToDynamicThunk(instruction);
+  } else if (custom_call_target == "GetRngSeed") {
+    return EmitRngSeedThunk(instruction);
   } else if (absl::StartsWith(custom_call->custom_call_target(), "__onednn$")) {
 #ifdef XLA_ONEDNN
     return EmitOneDnnOpThunk(instruction);

CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

tensorflow/compiler/aot/thunk_proto_execution_deserializer.cc
@@ -118,6 +118,11 @@ ThunkProtoExecutionDeserializer::ThunkSpecificRunImplFromThunkSequence(
                             GetTopKThunkRunImpl(thunk));
         break;
       }
+      case xla::cpu::ThunkProto::kRngSeedThunk: {
+        TF_ASSIGN_OR_RETURN(thunk_run_impls.emplace_back(),
+                            GetRngSeedThunkRunImpl(thunk));
+        break;
+      }
       default: {
         return xla::Internal("Unsupported thunk type: %s.", thunk.kind());
       }
@@ -428,6 +433,36 @@ ThunkProtoExecutionDeserializer::GetRngGetAndUpdateStateThunkRunImpl(
        {"{{RNG_STATE_PTR}}",
         absl::StrCat("reinterpret_cast<uint64_t*>(",
                      GetBufferAllocationString(rng_thunk.state_buffer()),
+                     ")")}});
+}
+
+absl::StatusOr<std::string>
+ThunkProtoExecutionDeserializer::GetRngSeedThunkRunImpl(
+    const xla::cpu::ThunkProto& thunk) {
+  if (!thunk.has_rng_seed_thunk()) {
+    return xla::Internal(
+        "RngSeed thunk was expected when getting thunk run implementation.");
+  }
+  const xla::cpu::RngSeedThunkProto& rng_seed_thunk = thunk.rng_seed_thunk();
+
+  absl::string_view rng_seed_thunk_invocation_format = R"(
+     // Rng Seed Thunk
+     {
+         uint64_t seed = static_cast<uint64_t>(run_options->rng_seed());
+         if (seed == 0) {
+           static thread_local std::random_device rd;
+           static thread_local std::mt19937_64 gen(rd());
+           std::uniform_int_distribution<uint64_t> distrib(1, std::numeric_limits<uint64_t>::max());
+           seed = distrib(gen);
+         }
+         *reinterpret_cast<uint64_t*>({{RNG_SEED_PTR}}) = seed;
+     })";
+
+  return absl::StrReplaceAll(
+      rng_seed_thunk_invocation_format,
+      {{"{{RNG_SEED_PTR}}",
+        absl::StrCat("reinterpret_cast<uint64_t*>(",
+                     GetBufferAllocationString(rng_seed_thunk.dest_buffer()),
                      ")")}});
 }
 

third_party/xla/xla/backends/cpu/runtime/rng_seed_thunk.cc
@@ -1,0 +1,86 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
+
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <utility>
+
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_format.h"
+#include "xla/backends/cpu/runtime/thunk.h"
+#include "xla/runtime/buffer_use.h"
+#include "xla/service/buffer_assignment.h"
+#include "xla/shape_util.h"
+#include "xla/stream_executor/device_address.h"
+#include "xla/tsl/concurrency/async_value_ref.h"
+#include "xla/util.h"
+#include "tsl/platform/random.h"
+
+namespace xla::cpu {
+
+absl::StatusOr<std::unique_ptr<RngSeedThunk>> RngSeedThunk::Create(
+    Info info, BufferAllocation::Slice dest_buffer) {
+  return std::make_unique<RngSeedThunk>(std::move(info), dest_buffer);
+}
+
+RngSeedThunk::RngSeedThunk(Info info, BufferAllocation::Slice dest_buffer)
+    : Thunk(Kind::kRngSeed, std::move(info)), dest_buffer_(dest_buffer) {}
+
+tsl::AsyncValueRef<Thunk::ExecuteEvent> RngSeedThunk::Execute(
+    const ExecuteParams& params) {
+  if (params.buffer_allocations == nullptr) {
+    return InvalidArgument("buffer_allocations cannot be null");
+  }
+  auto status_or_addr =
+      params.buffer_allocations->GetDeviceAddress(dest_buffer_);
+  if (!status_or_addr.ok()) {
+    return tsl::MakeErrorAsyncValueRef(status_or_addr.status());
+  }
+  se::DeviceAddressBase dest_data = *status_or_addr;
+  if (dest_data.opaque() == nullptr) {
+    return InvalidArgument("seed buffer opaque address cannot be null");
+  }
+
+  if (dest_data.size() != sizeof(uint64_t)) {
+    return InvalidArgument("Invalid seed buffer size: %u", dest_data.size());
+  }
+
+  uint64_t seed = static_cast<uint64_t>(params.rng_seed);
+  if (seed == 0) {
+    // 0 is avoided because it is used as a sentinel value indicating that a
+    // random seed should be generated.
+    do {
+      seed = tsl::random::New64();
+    } while (seed == 0);
+  }
+
+  VLOG(3) << absl::StreamFormat("RngSeedThunk: seed=%d", seed);
+  VLOG(3) << absl::StreamFormat("  dest: %s (%p)", dest_buffer_.ToString(),
+                                dest_data.opaque());
+
+  std::memcpy(dest_data.opaque(), &seed, sizeof(uint64_t));
+
+  return OkExecuteEvent();
+}
+
+Thunk::BufferUses RngSeedThunk::buffer_uses() const {
+  return {BufferUse::Write(dest_buffer_, ShapeUtil::MakeShape(U64, {}))};
+}
+
+}  // namespace xla::cpu

third_party/xla/xla/backends/cpu/runtime/thunk_proto_serdes.cc
@@ -25,6 +25,7 @@ limitations under the License.
 #include <vector>
 
 #include "absl/algorithm/container.h"
+#include "absl/base/casts.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/check.h"
@@ -41,6 +42,7 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/kernel_thunk.h"
 #include "xla/backends/cpu/runtime/logical_id_thunk.h"
 #include "xla/backends/cpu/runtime/outfeed_thunk.h"
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
 #include "xla/backends/cpu/runtime/rng_state_thunk.h"
 #include "xla/backends/cpu/runtime/serdes_base.h"
 #include "xla/backends/cpu/runtime/sort_thunk.h"
@@ -168,6 +170,8 @@ static absl::StatusOr<Thunk::Kind> ProtoThunkToThunkKind(
       return Thunk::Kind::kReplicaId;
     case ThunkProto::ImplCase::kYnnFusionThunk:
       return Thunk::Kind::kYnnFusion;
+    case ThunkProto::ImplCase::kRngSeedThunk:
+      return Thunk::Kind::kRngSeed;
     case ThunkProto::ImplCase::IMPL_NOT_SET:
       return Internal("Thunk kind not set.");
   }
@@ -446,6 +450,12 @@ static absl::Status ToProto(const ReplicaIdThunk& thunk, ThunkProto& proto) {
   return absl::OkStatus();
 }
 
+static absl::Status ToProto(const RngSeedThunk& thunk, ThunkProto& proto) {
+  ASSIGN_OR_RETURN(*proto.mutable_rng_seed_thunk()->mutable_dest_buffer(),
+                   thunk.dest_buffer().ToProto());
+  return absl::OkStatus();
+}
+
 absl::StatusOr<ThunkProto> ThunkSerDesProtobuf::ToProto(
     const Thunk& thunk) const {
   ThunkProto proto;
@@ -512,6 +522,10 @@ absl::StatusOr<ThunkProto> ThunkSerDesProtobuf::ToProto(
               absl::down_cast<const internal::LogicalIdThunk<
                   internal::LogicalIdKind::kReplicaId>&>(thunk)),
           proto));
+      break;
+    case Thunk::Kind::kRngSeed:
+      RETURN_IF_ERROR(::xla::cpu::ToProto(
+          absl::down_cast<const RngSeedThunk&>(thunk), proto));
       break;
     default:
       return absl::UnimplementedError(
@@ -825,6 +839,19 @@ static absl::StatusOr<std::unique_ptr<Thunk>> ReplicaIdThunkFromProto(
       std::move(info), std::move(logical_id_buffer));
 }
 
+static absl::StatusOr<std::unique_ptr<RngSeedThunk>> RngSeedThunkFromProto(
+    const ThunkProto& proto,
+    const std::vector<BufferAllocation>& buffer_allocations) {
+  ASSIGN_OR_RETURN(Thunk::Info info, ThunkInfoFromProto(proto.info()));
+
+  ASSIGN_OR_RETURN(
+      BufferAllocation::Slice dest_buffer,
+      BufferAllocation::Slice::FromProto(proto.rng_seed_thunk().dest_buffer(),
+                                         buffer_allocations));
+
+  return RngSeedThunk::Create(std::move(info), std::move(dest_buffer));
+}
+
 absl::StatusOr<std::unique_ptr<Thunk>> ThunkSerDesProtobuf::FromProto(
     const ThunkProto& proto) const {
   CHECK(buffer_allocations_ != nullptr);
@@ -867,6 +894,8 @@ absl::StatusOr<std::unique_ptr<Thunk>> ThunkSerDesProtobuf::FromProto(
       return PartitionIdThunkFromProto(proto, *buffer_allocations_);
     case Thunk::Kind::kReplicaId:
       return ReplicaIdThunkFromProto(proto, *buffer_allocations_);
+    case Thunk::Kind::kRngSeed:
+      return RngSeedThunkFromProto(proto, *buffer_allocations_);
     default:
       return absl::Status(absl::StatusCode::kInvalidArgument,
                           absl::StrFormat("Unsupported thunk kind: %s",

third_party/xla/xla/backends/cpu/runtime/thunk_sequence_serdes_test.cc
@@ -24,6 +24,7 @@ limitations under the License.
 
 #include <gtest/gtest.h>
 #include "absl/algorithm/container.h"
+#include "absl/base/casts.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/check.h"
@@ -50,6 +51,7 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/logical_id_thunk.h"
 #include "xla/backends/cpu/runtime/outfeed_thunk.h"
 #include "xla/backends/cpu/runtime/reduce_scatter_thunk.h"
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
 #include "xla/backends/cpu/runtime/rng_state_thunk.h"
 #include "xla/backends/cpu/runtime/serdes_base.h"
 #include "xla/backends/cpu/runtime/sort_thunk.h"
@@ -180,6 +182,7 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
     ASSIGN_OR_RETURN(thunk_sequence.emplace_back(), CreateKernelThunk());
     ASSIGN_OR_RETURN(thunk_sequence.emplace_back(), CreateConvolutionThunk());
     ASSIGN_OR_RETURN(thunk_sequence.emplace_back(), CreateSortThunk());
+    ASSIGN_OR_RETURN(thunk_sequence.emplace_back(), CreateRngSeedThunk());
     return thunk_sequence;
   }
 
@@ -674,6 +677,14 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
         /*direction=*/SortThunk::SortDirection::kAscending);
   }
 
+  absl::StatusOr<std::unique_ptr<Thunk>> CreateRngSeedThunk() {
+    RETURN_IF_ERROR(AddBufferAllocations(1));
+    return RngSeedThunk::Create(
+        Thunk::Info(),
+        CreateBufferAllocationSlice(
+            buffer_allocations_[buffer_allocations_.size() - 1]));
+  }
+
   bool VerifySliceEquality(const BufferAllocation::Slice& slice_1,
                            const BufferAllocation::Slice& slice_2) {
     return slice_1.offset() == slice_2.offset() &&
@@ -980,6 +991,11 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
            thunk_1.delta() == thunk_2.delta();
   }
 
+  bool VerifyRngSeedThunkEquality(const RngSeedThunk& thunk_1,
+                                  const RngSeedThunk& thunk_2) {
+    return VerifySliceEquality(thunk_1.dest_buffer(), thunk_2.dest_buffer());
+  }
+
   bool VerifySortThunkEquality(const SortThunk& thunk_1,
                                const SortThunk& thunk_2) {
     return thunk_1.comparator_name() == thunk_2.comparator_name() &&
@@ -1216,6 +1232,10 @@ class ThunkSequenceSerdesTest : public ::testing::Test {
         return VerifyRngGetAndUpdateStateThunkEquality(
             absl::down_cast<const RngGetAndUpdateStateThunk&>(thunk_1),
             absl::down_cast<const RngGetAndUpdateStateThunk&>(thunk_2));
+      case Thunk::Kind::kRngSeed:
+        return VerifyRngSeedThunkEquality(
+            absl::down_cast<const RngSeedThunk&>(thunk_1),
+            absl::down_cast<const RngSeedThunk&>(thunk_2));
       case Thunk::Kind::kSort:
         return VerifySortThunkEquality(
             absl::down_cast<const SortThunk&>(thunk_1),

third_party/xla/xla/service/cpu/thunk_emitter.cc
@@ -65,6 +65,7 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/logical_id_thunk.h"
 #include "xla/backends/cpu/runtime/outfeed_thunk.h"
 #include "xla/backends/cpu/runtime/reduce_scatter_thunk.h"
+#include "xla/backends/cpu/runtime/rng_seed_thunk.h"
 #include "xla/backends/cpu/runtime/rng_state_thunk.h"
 #include "xla/backends/cpu/runtime/sort_thunk.h"
 #include "xla/backends/cpu/runtime/thunk.h"
@@ -892,6 +893,12 @@ absl::StatusOr<ThunkSequence> ThunkEmitter::EmitRngGetAndUpdateStateThunk(
       ThunkInfo(instruction), state_buffer, rng_state->delta());
 }
 
+absl::StatusOr<ThunkSequence> ThunkEmitter::EmitRngSeedThunk(
+    const HloInstruction* instruction) {
+  ASSIGN_OR_RETURN(auto seed_buffer, GetAllocationSlice(instruction));
+  return ThunkSequence::Of<RngSeedThunk>(ThunkInfo(instruction), seed_buffer);
+}
+
 absl::StatusOr<ThunkSequence> ThunkEmitter::EmitStochasticConvertThunk(
     const HloInstruction* instruction) {
   return Unimplemented("StochasticConvert should be decomposed for CPU.");
@@ -1213,6 +1220,8 @@ absl::StatusOr<ThunkSequence> ThunkEmitter::EmitCustomCallThunk(
     return EmitTopKThunk(custom_call);
   } else if (custom_call_target == "SliceToDynamic") {
     return EmitSliceToDynamicThunk(instruction);
+  } else if (custom_call_target == "GetRngSeed") {
+    return EmitRngSeedThunk(instruction);
   } else if (absl::StartsWith(custom_call->custom_call_target(), "__onednn$")) {
 #ifdef XLA_ONEDNN
     return EmitOneDnnOpThunk(instruction);