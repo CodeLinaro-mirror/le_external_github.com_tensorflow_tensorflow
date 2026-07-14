109f9d7317bb03312ac3512d77bc976da923c8d3
Ilya Tikhonovskiy <loislo@google.com>
2026-07-14 01:04:48 -0700

    Rename xla_gpu_default_to_alg_dot_bf16_bf16_f32 to xla_gpu_precision_like_on_tpu.
    This renames the debug option flag to more accurately describe its effect: using a lower precision for dot operations on GPU, similar to the default precision used on TPU.
    
    PiperOrigin-RevId: 947507955

--- third_party/xla/xla/codegen/xtile/codegen/dot_algorithms.cc
+++ third_party/xla/xla/codegen/xtile/codegen/dot_algorithms.cc
@@ -209,7 +209,7 @@ absl::StatusOr<std::optional<Type>> DotDefaultOperandsType(
       return lhs_type;
     }
   }
-  if (debug_options.xla_gpu_default_to_alg_dot_bf16_bf16_f32()) {
+  if (debug_options.xla_gpu_precision_like_on_tpu()) {
     return b.getBF16Type();
   }
   return lhs_type;
--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -389,7 +389,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_experimental_enable_fusion_block_level_rewriter(false);
 
-  opts.set_xla_gpu_default_to_alg_dot_bf16_bf16_f32(false);
+  opts.set_xla_gpu_precision_like_on_tpu(false);
   opts.set_xla_gpu_enable_libnvptxcompiler(
       stream_executor::IsLibNvPtxCompilerSupported());
   opts.set_xla_gpu_libnvjitlink_mode(DebugOptions::LIB_NV_JIT_LINK_MODE_AUTO);
@@ -1862,11 +1862,10 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
 
   flag_list->push_back(tsl::Flag(
       "xla_gpu_default_to_alg_dot_bf16_bf16_f32",
-      bool_setter_for(
-          &DebugOptions::set_xla_gpu_default_to_alg_dot_bf16_bf16_f32),
-      debug_options->xla_gpu_default_to_alg_dot_bf16_bf16_f32(),
+      bool_setter_for(&DebugOptions::set_xla_gpu_precision_like_on_tpu),
+      debug_options->xla_gpu_precision_like_on_tpu(),
       "Use the dot precision algorithm `ALG_DOT_BF16_BF16_F32 by default for "
-      "f32 dots."));
+      "f32 dots. This leads to the same precision as on TPU."));
   flag_list->push_back(
       tsl::Flag("xla_gpu_deterministic_ops",
                 bool_setter_for(&DebugOptions::set_xla_gpu_deterministic_ops),
