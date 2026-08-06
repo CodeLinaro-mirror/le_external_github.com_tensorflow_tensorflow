CodeLinaro-mirror/lc_external_github.com_tensorflow_tensorflow

[XLA:GPU]: Integrate the collective fusion pass.
With this we don't form transient fusions for all-reduce anymore.
1. EmitCollectiveThunk is now gone from thunk_emitter. We directly emit in the TritonFusion emitter.
2. Some common parts for building collective buffers were extracted into CollectiveThunk so that both thunk_emitter and triton fusion emitter can use that.
3. Collective annotator and fusion pass is moved to before triton gemm fusions are formed. This also means plumbing in the gpu topology in a few places (gpu_compiler implementations).

PiperOrigin-RevId: 942145859

third_party/xla/xla/backends/gpu/codegen/triton/fusion.cc
@@ -26,10 +26,10 @@ limitations under the License.
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "xla/tsl/platform/status_macros.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Metadata.h"
 #include "llvm/IR/Module.h"
@@ -37,36 +37,97 @@ limitations under the License.
 #include "llvm/TargetParser/Triple.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/Support/LLVM.h"
-#include "xla/backends/gpu/codegen/fusion_emitter.h"
 #include "xla/backends/gpu/codegen/kernel_compiler.h"
 #include "xla/backends/gpu/codegen/kernels/custom_kernel.h"
 #include "xla/backends/gpu/codegen/kernels/ptx_custom_kernel.h"
+#include "xla/backends/gpu/codegen/triton/collective_emitter.h"
 #include "xla/backends/gpu/codegen/triton/triton_kernel_source.h"
 #include "xla/backends/gpu/codegen/triton/xtile_compiler.h"
+#include "xla/backends/gpu/runtime/collective_kernel_thunk.h"
+#include "xla/backends/gpu/runtime/collective_params.h"
+#include "xla/backends/gpu/runtime/collective_thunk.h"
 #include "xla/backends/gpu/runtime/custom_kernel_thunk.h"
 #include "xla/backends/gpu/runtime/thunk.h"
 #include "xla/codegen/emitters/kernel_arguments.h"
-#include "xla/codegen/llvm_kernel_source.h"
 #include "xla/frontend_attributes.h"
 #include "xla/future.h"
-#include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/utils/hlo_traversal.h"
 #include "xla/service/gpu/backend_configs.pb.h"
 #include "xla/service/gpu/gpu_constants.h"
+#include "xla/service/gpu/hlo_fusion_analysis.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/service/gpu/ir_emitter_context.h"
 #include "xla/service/gpu/kernel_reuse_cache.h"
 #include "xla/service/gpu/launch_dimensions.h"
 #include "xla/service/gpu/model/block_level_parameters.h"
 #include "xla/shape.h"
+#include "xla/status_macros.h"
 #include "xla/stream_executor/device_description.h"
-#include "xla/stream_executor/launch_dim.h"
 #include "xla/util.h"
 
 namespace xla {
 namespace gpu {
+
+namespace {
+bool IsGPUSyncCollective(const HloInstruction& instr) {
+  auto backend_config = instr.backend_config<GpuBackendConfig>();
+  if (!backend_config.ok()) {
+    return false;
+  }
+  return backend_config->collective_backend_config().is_sync();
+}
+
+struct EmitArgs {
+  TritonFusion::EmitThunk emit_thunk;
+  std::vector<Shape> unmanaged_arguments;
+};
+
+absl::StatusOr<EmitArgs> EmitCollectiveFusion(
+    Thunk::ThunkInfo info, const HloFusionAnalysis& analysis,
+    IrEmitterContext& ir_emitter_context, const HloFusionInstruction& fusion) {
+  VLOG(5) << "EmitCollectiveFusion: " << info.profile_annotation;
+  TF_RET_CHECK(analysis.fusion_heroes().size() == 1)
+      << "EmitCollectiveFusion: Expected exactly one hero in a collective "
+         "fusion. Found: "
+      << analysis.fusion_heroes().size() << " for: " << fusion.ToString();
+
+  const HloInstructionAdaptor collective_hero = analysis.fusion_hero(0);
+
+  bool use_global_device_ids = collective_hero.use_global_device_ids();
+
+  CollectiveConfig config = GetCollectiveConfig(&collective_hero.instruction(),
+                                                use_global_device_ids);
+  const HloFusionInstruction* fusion_instr = &fusion;
+  std::vector<CollectiveThunk::Buffer> buffers;
+  ASSIGN_OR_RETURN(buffers, GetCollectiveBuffers(
+                                ir_emitter_context.buffer_assignment(),
+                                fusion_instr, Thunk::Kind::kCollectiveKernel,
+                                /*has_dynamic_root=*/false));
+  const bool is_async = !IsGPUSyncCollective(*fusion_instr);
+
+  TritonFusion::EmitThunk make_thunk =
+      [info = std::move(info), buffers = std::move(buffers), config,
+       fusion_instr, is_async](TritonFusion::EmitResult result) mutable
+      -> absl::StatusOr<ThunkSequence> {
+    ASSIGN_OR_RETURN(CollectiveKernelSpec kernel_spec,
+                     CreateCollectiveKernelSpec(
+                         fusion_instr, result.entry.launch_dimensions));
+    auto cubin = result.entry.binary.empty()
+                     ? std::nullopt
+                     : std::make_optional(std::move(result.entry.binary));
+    return ThunkSequence::Of<CollectiveKernelThunk>(
+        std::move(info), config, std::move(kernel_spec), is_async,
+        std::move(buffers), /*is_collective_kernel_enabled=*/true,
+        result.entry.kernel_name, result.entry.launch_dimensions,
+        result.entry.shmem_bytes, std::move(cubin), result.entry.use_pdl);
+  };
+  ASSIGN_OR_RETURN(std::vector<Shape> unmanaged_arguments,
+                   GetCollectiveUnmanagedKernelArguments(fusion_instr));
+  return EmitArgs{std::move(make_thunk), std::move(unmanaged_arguments)};
+}
+}  // namespace
 
 // Since we are creating the kernel and splicing the impl_fn into it, we
 // need to manually annotate the kernel with the nvvm.annotations.
@@ -128,23 +189,37 @@ AsyncThunkSequence TritonFusion::Emit(
     const HloFusionInstruction& fusion) const {
   Thunk::ThunkInfo thunk_info = Thunk::ThunkInfo::WithProfileAnnotation(
       &fusion, ir_emitter_context.GetNextThunkId());
-  return Emit(ir_emitter_context, fusion, nullptr, {})
-      .Map(
-          [thunk_info = std::move(thunk_info)](
-              EmitResult result) -> absl::StatusOr<ThunkSequence> {
-            ASSIGN_OR_RETURN(
-                CustomKernel custom_kernel,
-                kernel::CreateOwnedCubinCustomKernel(
-                    result.entry.kernel_name, result.entry.binary,
-                    result.kernel_arguments.args().size(),
-                    result.entry.launch_dimensions.block_counts(),
-                    result.entry.launch_dimensions.thread_counts_per_block(),
-                    result.entry.shmem_bytes));
-            return ThunkSequence::Of<CustomKernelThunk>(
-                thunk_info, std::move(custom_kernel), result.kernel_arguments,
-                result.entry.use_pdl, std::vector<int64_t>{},
-                result.entry.tma_metadata);
-          });
+  VLOG(5) << "TritonFusion::Emit: " << thunk_info.profile_annotation;
+  EmitArgs emit_args;
+  if (analysis_.fusion_backend_config().kind() == kTritonCollectiveFusionKind) {
+    ASSIGN_OR_RETURN(emit_args,
+                     EmitCollectiveFusion(std::move(thunk_info), analysis_,
+                                          ir_emitter_context, fusion));
+  } else {
+    EmitThunk make_thunk =
+        [thunk_info = std::move(thunk_info)](
+            EmitResult result) -> absl::StatusOr<ThunkSequence> {
+      ASSIGN_OR_RETURN(
+          CustomKernel custom_kernel,
+          kernel::CreateOwnedCubinCustomKernel(
+              result.entry.kernel_name, result.entry.binary,
+              result.kernel_arguments.args().size(),
+              result.entry.launch_dimensions.block_counts(),
+              result.entry.launch_dimensions.thread_counts_per_block(),
+              result.entry.shmem_bytes));
+      return ThunkSequence::Of<CustomKernelThunk>(
+          thunk_info, std::move(custom_kernel), result.kernel_arguments,
+          result.entry.use_pdl, std::vector<int64_t>{},
+          result.entry.tma_metadata);
+    };
+    emit_args = {
+        std::move(make_thunk),
+        /*unmanaged_arguments=*/{},
+    };
+  }
+  return Emit(ir_emitter_context, fusion, nullptr,
+              std::move(emit_args.unmanaged_arguments))
+      .Map(std::move(emit_args.emit_thunk));
 }
 
 xla::Future<TritonFusion::EmitResult> TritonFusion::Emit(

third_party/xla/xla/backends/gpu/runtime/collective_thunk.cc
@@ -45,6 +45,8 @@ limitations under the License.
 #include "xla/core/collectives/rank_id.h"
 #include "xla/debug_options_flags.h"
 #include "xla/hlo/ir/collective_op_group_mode.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/primitive_util.h"
 #include "xla/runtime/buffer_use.h"
 #include "xla/runtime/device_id.h"
@@ -55,13 +57,13 @@ limitations under the License.
 #include "xla/service/rendezvous.h"
 #include "xla/service/shaped_slice.h"
 #include "xla/shape.h"
+#include "xla/shape_util.h"
 #include "xla/status_macros.h"
 #include "xla/stream_executor/command_buffer.h"
 #include "xla/stream_executor/platform.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/stream_executor/stream_executor.h"
 #include "xla/stream_executor/trace_command_buffer_factory.h"
-#include "xla/tsl/platform/errors.h"
 #include "xla/util.h"
 #include "xla/xla.pb.h"
 #include "xla/xla_data.pb.h"
@@ -101,6 +103,25 @@ bool IsTypeSupportedBy(PrimitiveType element_type, Thunk::Kind reduction_op) {
     default:
       return false;
   }
+}
+
+ShapeIndex GetCollectiveResultShapeIndex(const HloInstruction* collective,
+                                         int64_t operand_index) {
+  const bool has_nested_result =
+      HloPredicateIsOp<HloOpcode::kAllGatherStart,
+                       HloOpcode::kCollectivePermuteStart>(collective);
+  const Shape& result_shape = has_nested_result
+                                  ? collective->shape().tuple_shapes(1)
+                                  : collective->shape();
+
+  ShapeIndex result_index;
+  if (has_nested_result) {
+    result_index.push_back(1);
+  }
+  if (result_shape.IsTuple()) {
+    result_index.push_back(operand_index);
+  }
+  return result_index;
 }
 
 }  // namespace
@@ -251,6 +272,78 @@ absl::StatusOr<std::vector<DeviceBufferPair>> ConvertToDeviceBuffers(
         buffers[i].source_memory_space, buffers[i].destination_memory_space});
   }
   return device_buffers;
+}
+
+absl::StatusOr<std::vector<CollectiveThunk::Buffer>> GetCollectiveBuffers(
+    const BufferAssignment& buffer_assignment, const HloInstruction* inst,
+    Thunk::Kind kind, const bool has_dynamic_root) {
+  int64_t operand_count = inst->operand_count();
+  std::vector<CollectiveThunk::Buffer> buffers;
+  buffers.reserve(operand_count);
+
+  // Adds a source and destination buffers pair to `buffers`.
+  auto add_buffer = [&](const HloInstruction* src, const HloInstruction* dst,
+                        const ShapeIndex& dst_shape_index) -> absl::Status {
+    const Shape& src_shape = src->shape();
+    const Shape& dst_shape =
+        ShapeUtil::GetSubshape(dst->shape(), dst_shape_index);
+
+    ASSIGN_OR_RETURN(auto src_slice,
+                     buffer_assignment.GetUniqueSlice(src, /*index=*/{}));
+    ASSIGN_OR_RETURN(auto dst_slice,
+                     buffer_assignment.GetUniqueSlice(dst, dst_shape_index));
+
+    buffers.push_back(CollectiveThunk::Buffer{
+        /*element_count=*/ShapeUtil::ElementsIn(src_shape),
+        /*source_buffer=*/{src_slice, src_shape},
+        /*destination_buffer=*/{dst_slice, dst_shape},
+        /*source_memory_space=*/src_shape.layout().memory_space(),
+        /*destination_memory_space=*/dst_shape.layout().memory_space()});
+    return absl::OkStatus();
+  };
+
+  if (kind == Thunk::Kind::kAllGather) {
+    // Start operations return a tuple of (<<inputs>>, <<outputs>>)
+    // where outputs can be a tuple itself (if operation has
+    // multiple operands).
+    for (int64_t i = 0; i < operand_count; i++) {
+      ShapeIndex idx = GetCollectiveResultShapeIndex(inst, i);
+      RETURN_IF_ERROR(add_buffer(inst->operand(i), inst, idx));
+    }
+  } else if (kind == Thunk::Kind::kRaggedAllToAll) {
+    // RaggedAllToAll operation has 6 operands: input, output,
+    // input_offset, send_size, output_offset, recv_size. `output`
+    // operand is aliased with the instruction result. All other
+    // operands are not aliased.
+    RETURN_IF_ERROR(
+        add_buffer(inst->operand(0), inst->operand(0), ShapeIndex({})));
+    RETURN_IF_ERROR(add_buffer(inst->operand(1), inst,
+                               GetCollectiveResultShapeIndex(inst, 0)));
+
+    for (int64_t i = 2; i < operand_count; i++) {
+      RETURN_IF_ERROR(
+          add_buffer(inst->operand(i), inst->operand(i), ShapeIndex({})));
+    }
+  } else {
+    // For other operations simply zip operands with results.
+    //
+    // A collective-broadcast with a dynamic root carries an extra trailing
+    // operand: a 1-D S32 vector holding the runtime-selected root rank for each
+    // data operand. That operand has no corresponding output, so it is not
+    // zipped with a result; instead it is mapped to its own allocation and
+    // consumed separately by the thunk.
+    int64_t num_data_operands =
+        has_dynamic_root ? operand_count - 1 : operand_count;
+    for (int64_t i = 0; i < num_data_operands; i++) {
+      ShapeIndex idx = GetCollectiveResultShapeIndex(inst, i);
+      RETURN_IF_ERROR(add_buffer(inst->operand(i), inst, idx));
+    }
+    if (has_dynamic_root) {
+      const HloInstruction* roots = inst->operand(operand_count - 1);
+      RETURN_IF_ERROR(add_buffer(roots, roots, ShapeIndex({})));
+    }
+  }
+  return buffers;
 }
 
 absl::StatusOr<CollectiveBufferProto> CollectiveThunk::Buffer::ToProto() const {

CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

third_party/xla/xla/backends/gpu/codegen/triton/fusion.cc
@@ -26,10 +26,10 @@ limitations under the License.
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "xla/tsl/platform/status_macros.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Metadata.h"
 #include "llvm/IR/Module.h"
@@ -37,36 +37,97 @@ limitations under the License.
 #include "llvm/TargetParser/Triple.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/Support/LLVM.h"
-#include "xla/backends/gpu/codegen/fusion_emitter.h"
 #include "xla/backends/gpu/codegen/kernel_compiler.h"
 #include "xla/backends/gpu/codegen/kernels/custom_kernel.h"
 #include "xla/backends/gpu/codegen/kernels/ptx_custom_kernel.h"
+#include "xla/backends/gpu/codegen/triton/collective_emitter.h"
 #include "xla/backends/gpu/codegen/triton/triton_kernel_source.h"
 #include "xla/backends/gpu/codegen/triton/xtile_compiler.h"
+#include "xla/backends/gpu/runtime/collective_kernel_thunk.h"
+#include "xla/backends/gpu/runtime/collective_params.h"
+#include "xla/backends/gpu/runtime/collective_thunk.h"
 #include "xla/backends/gpu/runtime/custom_kernel_thunk.h"
 #include "xla/backends/gpu/runtime/thunk.h"
 #include "xla/codegen/emitters/kernel_arguments.h"
-#include "xla/codegen/llvm_kernel_source.h"
 #include "xla/frontend_attributes.h"
 #include "xla/future.h"
-#include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/utils/hlo_traversal.h"
 #include "xla/service/gpu/backend_configs.pb.h"
 #include "xla/service/gpu/gpu_constants.h"
+#include "xla/service/gpu/hlo_fusion_analysis.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/service/gpu/ir_emitter_context.h"
 #include "xla/service/gpu/kernel_reuse_cache.h"
 #include "xla/service/gpu/launch_dimensions.h"
 #include "xla/service/gpu/model/block_level_parameters.h"
 #include "xla/shape.h"
+#include "xla/status_macros.h"
 #include "xla/stream_executor/device_description.h"
-#include "xla/stream_executor/launch_dim.h"
 #include "xla/util.h"
 
 namespace xla {
 namespace gpu {
+
+namespace {
+bool IsGPUSyncCollective(const HloInstruction& instr) {
+  auto backend_config = instr.backend_config<GpuBackendConfig>();
+  if (!backend_config.ok()) {
+    return false;
+  }
+  return backend_config->collective_backend_config().is_sync();
+}
+
+struct EmitArgs {
+  TritonFusion::EmitThunk emit_thunk;
+  std::vector<Shape> unmanaged_arguments;
+};
+
+absl::StatusOr<EmitArgs> EmitCollectiveFusion(
+    Thunk::ThunkInfo info, const HloFusionAnalysis& analysis,
+    IrEmitterContext& ir_emitter_context, const HloFusionInstruction& fusion) {
+  VLOG(5) << "EmitCollectiveFusion: " << info.profile_annotation;
+  TF_RET_CHECK(analysis.fusion_heroes().size() == 1)
+      << "EmitCollectiveFusion: Expected exactly one hero in a collective "
+         "fusion. Found: "
+      << analysis.fusion_heroes().size() << " for: " << fusion.ToString();
+
+  const HloInstructionAdaptor collective_hero = analysis.fusion_hero(0);
+
+  bool use_global_device_ids = collective_hero.use_global_device_ids();
+
+  CollectiveConfig config = GetCollectiveConfig(&collective_hero.instruction(),
+                                                use_global_device_ids);
+  const HloFusionInstruction* fusion_instr = &fusion;
+  std::vector<CollectiveThunk::Buffer> buffers;
+  ABSL_ASSIGN_OR_RETURN(buffers, GetCollectiveBuffers(
+                                ir_emitter_context.buffer_assignment(),
+                                fusion_instr, Thunk::Kind::kCollectiveKernel,
+                                /*has_dynamic_root=*/false));
+  const bool is_async = !IsGPUSyncCollective(*fusion_instr);
+
+  TritonFusion::EmitThunk make_thunk =
+      [info = std::move(info), buffers = std::move(buffers), config,
+       fusion_instr, is_async](TritonFusion::EmitResult result) mutable
+      -> absl::StatusOr<ThunkSequence> {
+    ABSL_ASSIGN_OR_RETURN(CollectiveKernelSpec kernel_spec,
+                     CreateCollectiveKernelSpec(
+                         fusion_instr, result.entry.launch_dimensions));
+    auto cubin = result.entry.binary.empty()
+                     ? std::nullopt
+                     : std::make_optional(std::move(result.entry.binary));
+    return ThunkSequence::Of<CollectiveKernelThunk>(
+        std::move(info), config, std::move(kernel_spec), is_async,
+        std::move(buffers), /*is_collective_kernel_enabled=*/true,
+        result.entry.kernel_name, result.entry.launch_dimensions,
+        result.entry.shmem_bytes, std::move(cubin), result.entry.use_pdl);
+  };
+  ABSL_ASSIGN_OR_RETURN(std::vector<Shape> unmanaged_arguments,
+                   GetCollectiveUnmanagedKernelArguments(fusion_instr));
+  return EmitArgs{std::move(make_thunk), std::move(unmanaged_arguments)};
+}
+}  // namespace
 
 // Since we are creating the kernel and splicing the impl_fn into it, we
 // need to manually annotate the kernel with the nvvm.annotations.
@@ -128,23 +189,37 @@ AsyncThunkSequence TritonFusion::Emit(
     const HloFusionInstruction& fusion) const {
   Thunk::ThunkInfo thunk_info = Thunk::ThunkInfo::WithProfileAnnotation(
       &fusion, ir_emitter_context.GetNextThunkId());
-  return Emit(ir_emitter_context, fusion, nullptr, {})
-      .Map(
-          [thunk_info = std::move(thunk_info)](
-              EmitResult result) -> absl::StatusOr<ThunkSequence> {
-            ABSL_ASSIGN_OR_RETURN(
-                CustomKernel custom_kernel,
-                kernel::CreateOwnedCubinCustomKernel(
-                    result.entry.kernel_name, result.entry.binary,
-                    result.kernel_arguments.args().size(),
-                    result.entry.launch_dimensions.block_counts(),
-                    result.entry.launch_dimensions.thread_counts_per_block(),
-                    result.entry.shmem_bytes));
-            return ThunkSequence::Of<CustomKernelThunk>(
-                thunk_info, std::move(custom_kernel), result.kernel_arguments,
-                result.entry.use_pdl, std::vector<int64_t>{},
-                result.entry.tma_metadata);
-          });
+  VLOG(5) << "TritonFusion::Emit: " << thunk_info.profile_annotation;
+  EmitArgs emit_args;
+  if (analysis_.fusion_backend_config().kind() == kTritonCollectiveFusionKind) {
+    ABSL_ASSIGN_OR_RETURN(emit_args,
+                     EmitCollectiveFusion(std::move(thunk_info), analysis_,
+                                          ir_emitter_context, fusion));
+  } else {
+    EmitThunk make_thunk =
+        [thunk_info = std::move(thunk_info)](
+            EmitResult result) -> absl::StatusOr<ThunkSequence> {
+      ABSL_ASSIGN_OR_RETURN(
+          CustomKernel custom_kernel,
+          kernel::CreateOwnedCubinCustomKernel(
+              result.entry.kernel_name, result.entry.binary,
+              result.kernel_arguments.args().size(),
+              result.entry.launch_dimensions.block_counts(),
+              result.entry.launch_dimensions.thread_counts_per_block(),
+              result.entry.shmem_bytes));
+      return ThunkSequence::Of<CustomKernelThunk>(
+          thunk_info, std::move(custom_kernel), result.kernel_arguments,
+          result.entry.use_pdl, std::vector<int64_t>{},
+          result.entry.tma_metadata);
+    };
+    emit_args = {
+        std::move(make_thunk),
+        /*unmanaged_arguments=*/{},
+    };
+  }
+  return Emit(ir_emitter_context, fusion, nullptr,
+              std::move(emit_args.unmanaged_arguments))
+      .Map(std::move(emit_args.emit_thunk));
 }
 
 xla::Future<TritonFusion::EmitResult> TritonFusion::Emit(

third_party/xla/xla/backends/gpu/runtime/collective_thunk.cc
@@ -45,6 +45,8 @@ limitations under the License.
 #include "xla/core/collectives/rank_id.h"
 #include "xla/debug_options_flags.h"
 #include "xla/hlo/ir/collective_op_group_mode.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/primitive_util.h"
 #include "xla/runtime/buffer_use.h"
 #include "xla/runtime/device_id.h"
@@ -55,13 +57,13 @@ limitations under the License.
 #include "xla/service/rendezvous.h"
 #include "xla/service/shaped_slice.h"
 #include "xla/shape.h"
+#include "xla/shape_util.h"
 #include "xla/status_macros.h"
 #include "xla/stream_executor/command_buffer.h"
 #include "xla/stream_executor/platform.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/stream_executor/stream_executor.h"
 #include "xla/stream_executor/trace_command_buffer_factory.h"
-#include "xla/tsl/platform/errors.h"
 #include "xla/util.h"
 #include "xla/xla.pb.h"
 #include "xla/xla_data.pb.h"
@@ -101,6 +103,25 @@ bool IsTypeSupportedBy(PrimitiveType element_type, Thunk::Kind reduction_op) {
     default:
       return false;
   }
+}
+
+ShapeIndex GetCollectiveResultShapeIndex(const HloInstruction* collective,
+                                         int64_t operand_index) {
+  const bool has_nested_result =
+      HloPredicateIsOp<HloOpcode::kAllGatherStart,
+                       HloOpcode::kCollectivePermuteStart>(collective);
+  const Shape& result_shape = has_nested_result
+                                  ? collective->shape().tuple_shapes(1)
+                                  : collective->shape();
+
+  ShapeIndex result_index;
+  if (has_nested_result) {
+    result_index.push_back(1);
+  }
+  if (result_shape.IsTuple()) {
+    result_index.push_back(operand_index);
+  }
+  return result_index;
 }
 
 }  // namespace
@@ -251,6 +272,78 @@ absl::StatusOr<std::vector<DeviceBufferPair>> ConvertToDeviceBuffers(
         buffers[i].source_memory_space, buffers[i].destination_memory_space});
   }
   return device_buffers;
+}
+
+absl::StatusOr<std::vector<CollectiveThunk::Buffer>> GetCollectiveBuffers(
+    const BufferAssignment& buffer_assignment, const HloInstruction* inst,
+    Thunk::Kind kind, const bool has_dynamic_root) {
+  int64_t operand_count = inst->operand_count();
+  std::vector<CollectiveThunk::Buffer> buffers;
+  buffers.reserve(operand_count);
+
+  // Adds a source and destination buffers pair to `buffers`.
+  auto add_buffer = [&](const HloInstruction* src, const HloInstruction* dst,
+                        const ShapeIndex& dst_shape_index) -> absl::Status {
+    const Shape& src_shape = src->shape();
+    const Shape& dst_shape =
+        ShapeUtil::GetSubshape(dst->shape(), dst_shape_index);
+
+    ABSL_ASSIGN_OR_RETURN(auto src_slice,
+                     buffer_assignment.GetUniqueSlice(src, /*index=*/{}));
+    ABSL_ASSIGN_OR_RETURN(auto dst_slice,
+                     buffer_assignment.GetUniqueSlice(dst, dst_shape_index));
+
+    buffers.push_back(CollectiveThunk::Buffer{
+        /*element_count=*/ShapeUtil::ElementsIn(src_shape),
+        /*source_buffer=*/{src_slice, src_shape},
+        /*destination_buffer=*/{dst_slice, dst_shape},
+        /*source_memory_space=*/src_shape.layout().memory_space(),
+        /*destination_memory_space=*/dst_shape.layout().memory_space()});
+    return absl::OkStatus();
+  };
+
+  if (kind == Thunk::Kind::kAllGather) {
+    // Start operations return a tuple of (<<inputs>>, <<outputs>>)
+    // where outputs can be a tuple itself (if operation has
+    // multiple operands).
+    for (int64_t i = 0; i < operand_count; i++) {
+      ShapeIndex idx = GetCollectiveResultShapeIndex(inst, i);
+      ABSL_RETURN_IF_ERROR(add_buffer(inst->operand(i), inst, idx));
+    }
+  } else if (kind == Thunk::Kind::kRaggedAllToAll) {
+    // RaggedAllToAll operation has 6 operands: input, output,
+    // input_offset, send_size, output_offset, recv_size. `output`
+    // operand is aliased with the instruction result. All other
+    // operands are not aliased.
+    ABSL_RETURN_IF_ERROR(
+        add_buffer(inst->operand(0), inst->operand(0), ShapeIndex({})));
+    ABSL_RETURN_IF_ERROR(add_buffer(inst->operand(1), inst,
+                               GetCollectiveResultShapeIndex(inst, 0)));
+
+    for (int64_t i = 2; i < operand_count; i++) {
+      ABSL_RETURN_IF_ERROR(
+          add_buffer(inst->operand(i), inst->operand(i), ShapeIndex({})));
+    }
+  } else {
+    // For other operations simply zip operands with results.
+    //
+    // A collective-broadcast with a dynamic root carries an extra trailing
+    // operand: a 1-D S32 vector holding the runtime-selected root rank for each
+    // data operand. That operand has no corresponding output, so it is not
+    // zipped with a result; instead it is mapped to its own allocation and
+    // consumed separately by the thunk.
+    int64_t num_data_operands =
+        has_dynamic_root ? operand_count - 1 : operand_count;
+    for (int64_t i = 0; i < num_data_operands; i++) {
+      ShapeIndex idx = GetCollectiveResultShapeIndex(inst, i);
+      ABSL_RETURN_IF_ERROR(add_buffer(inst->operand(i), inst, idx));
+    }
+    if (has_dynamic_root) {
+      const HloInstruction* roots = inst->operand(operand_count - 1);
+      ABSL_RETURN_IF_ERROR(add_buffer(roots, roots, ShapeIndex({})));
+    }
+  }
+  return buffers;
 }
 
 absl::StatusOr<CollectiveBufferProto> CollectiveThunk::Buffer::ToProto() const {