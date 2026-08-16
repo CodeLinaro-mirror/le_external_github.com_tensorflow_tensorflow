openxla/xla

Add backward compatibility verification to AOT test client
This introduces LoadSerializedArtifact() into AOTInterceptionPjrtClient, allowing it to skip the compilation stage and load the legacy GPU executable payloads and test our backwards compatibility guarantees.

PiperOrigin-RevId: 953315402

xla/tests/aot_interception_pjrt_client.cc
@@ -18,34 +18,134 @@ limitations under the License.
 #include <cstdint>
 #include <memory>
 #include <optional>
+#include <string>
 #include <utility>
 
 #include "absl/functional/any_invocable.h"
+#include "absl/status/status.h"
+#include "absl/status/status_macros.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
+#include "riegeli/bytes/string_writer.h"
 #include "xla/hlo/builder/xla_computation.h"
 #include "xla/layout.h"
 #include "xla/literal.h"
 #include "xla/pjrt/pjrt_client.h"
 #include "xla/pjrt/pjrt_executable.h"
+#include "xla/pjrt/proto/compile_options.pb.h"
 #include "xla/runtime/device_id.h"
 #include "xla/service/computation_placer.h"
+#include "xla/service/gpu/gpu_executable.pb.h"
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/logging.h"
+#include "xla/util/split_proto/human_readable_aot_executable.pb.h"
+#include "xla/util/split_proto/split_executable_and_options_writer.h"
+#include "xla/util/split_proto/split_gpu_executable_writer.h"
 #include "xla/xla_data.pb.h"
+#include "tsl/platform/protobuf.h"
 
 namespace xla {
+
+// Reads serialized executable and packs it back to ExecutableAndOptions in
+// riegeli format.
+absl::StatusOr<std::string>
+AOTInterceptionPjrtClient::LoadSerializedArtifact() {
+  LOG(INFO) << "AOTInterceptionPjrtClient: Loading serialized executable "
+               "from: "
+            << artifact_path_;
+  std::string text_proto;
+  RETURN_IF_ERROR(
+      tsl::ReadFileToString(tsl::Env::Default(), artifact_path_, &text_proto));
+
+  HumanReadableAotExecutable human_readable;
+  if (!tsl::protobuf::TextFormat::ParseFromString(text_proto,
+                                                  &human_readable)) {
+    return absl::InternalError(absl::StrCat(
+        "Failed to parse HumanReadableAotExecutable from ", artifact_path_));
+  }
+
+  LOG(INFO) << "AOTInterceptionPjrtClient: Parsed HumanReadableAotExecutable.";
+
+  ExecutableAndOptionsProto executable_and_options =
+      std::move(*human_readable.mutable_executable_and_options());
+
+  std::string serialized_gpu_exec;
+  RETURN_IF_ERROR(WriteSplitGpuExecutable(
+      human_readable.gpu_executable(),
+      std::make_unique<riegeli::StringWriter<>>(&serialized_gpu_exec)));
+  *executable_and_options.mutable_serialized_executable() =
+      std::move(serialized_gpu_exec);
+
+  std::string serialized;
+  RETURN_IF_ERROR(WriteSplitExecutableAndOptions(
+      executable_and_options,
+      std::make_unique<riegeli::StringWriter<>>(&serialized)));
+  LOG(INFO) << "AOTInterceptionPjrtClient: Successfully packed AOT artifact "
+               "into ExecutableAndOptions ("
+            << serialized.size() << " bytes).";
+  return serialized;
+}
 
 absl::StatusOr<std::unique_ptr<PjRtExecutable>>
 AOTInterceptionPjrtClient::Compile(const XlaComputation& computation,
                                    CompileOptions options) {
-  // Skeleton implementation: directly delegate to the underlying client.
+  if (mode_ == AOTTestMode::kBackwardsCompatibility) {
+    LOG(INFO) << "AOTInterceptionPjrtClient: Intercepting Compile in "
+                 "kBackwardsCompatibility mode for computation ["
+              << computation.name()
+              << "]. Deserializing executable instead of recompiling.";
+    ASSIGN_OR_RETURN(std::string serialized, LoadSerializedArtifact());
+    LOG(INFO) << "AOTInterceptionPjrtClient: Calling "
+                 "inner_client_->DeserializeExecutable.";
+    auto exec_or =
+        inner_client_->DeserializeExecutable(serialized, std::move(options));
+    if (exec_or.ok()) {
+      LOG(INFO) << "AOTInterceptionPjrtClient: Successfully deserialized "
+                   "executable for ["
+                << computation.name() << "]";
+    } else {
+      LOG(ERROR) << "AOTInterceptionPjrtClient: Failed to deserialize "
+                    "executable for ["
+                 << computation.name() << "]: " << exec_or.status();
+    }
+    return exec_or;
+  }
+  LOG(INFO) << "AOTInterceptionPjrtClient: Compile called in "
+               "kGoldenVerification mode for computation ["
+            << computation.name() << "]. Delegating to underlying client.";
   return inner_client_->Compile(computation, std::move(options));
 }
 
 absl::StatusOr<std::unique_ptr<PjRtLoadedExecutable>>
 AOTInterceptionPjrtClient::CompileAndLoad(const XlaComputation& computation,
                                           CompileOptions options) {
-  // Skeleton implementation: directly delegate to the underlying client.
+  if (mode_ == AOTTestMode::kBackwardsCompatibility) {
+    LOG(INFO) << "AOTInterceptionPjrtClient: Intercepting CompileAndLoad in "
+                 "kBackwardsCompatibility mode for computation ["
+              << computation.name()
+              << "]. Deserializing and loading executable instead of "
+                 "recompiling.";
+    ASSIGN_OR_RETURN(std::string serialized, LoadSerializedArtifact());
+    LOG(INFO) << "AOTInterceptionPjrtClient: Calling "
+                 "inner_client_->LoadSerializedExecutable.";
+    auto loaded_or = inner_client_->LoadSerializedExecutable(
+        serialized, std::move(options), LoadOptions());
+    if (loaded_or.ok()) {
+      LOG(INFO) << "AOTInterceptionPjrtClient: Successfully deserialized and "
+                   "loaded executable for ["
+                << computation.name() << "]";
+    } else {
+      LOG(ERROR) << "AOTInterceptionPjrtClient: Failed to load serialized "
+                    "executable for ["
+                 << computation.name() << "]: " << loaded_or.status();
+    }
+    return loaded_or;
+  }
+  LOG(INFO) << "AOTInterceptionPjrtClient: CompileAndLoad called in "
+               "kGoldenVerification mode for computation ["
+            << computation.name() << "]. Delegating to underlying client.";
   return inner_client_->CompileAndLoad(computation, std::move(options));
 }
 

CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

third_party/xla/xla/tests/aot_interception_pjrt_client.cc
@@ -18,34 +18,134 @@ limitations under the License.
 #include <cstdint>
 #include <memory>
 #include <optional>
+#include <string>
 #include <utility>
 
 #include "absl/functional/any_invocable.h"
+#include "absl/status/status.h"
+#include "absl/status/status_macros.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
+#include "riegeli/bytes/string_writer.h"
 #include "xla/hlo/builder/xla_computation.h"
 #include "xla/layout.h"
 #include "xla/literal.h"
 #include "xla/pjrt/pjrt_client.h"
 #include "xla/pjrt/pjrt_executable.h"
+#include "xla/pjrt/proto/compile_options.pb.h"
 #include "xla/runtime/device_id.h"
 #include "xla/service/computation_placer.h"
+#include "xla/service/gpu/gpu_executable.pb.h"
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/logging.h"
+#include "xla/util/split_proto/human_readable_aot_executable.pb.h"
+#include "xla/util/split_proto/split_executable_and_options_writer.h"
+#include "xla/util/split_proto/split_gpu_executable_writer.h"
 #include "xla/xla_data.pb.h"
+#include "tsl/platform/protobuf.h"
 
 namespace xla {
+
+// Reads serialized executable and packs it back to ExecutableAndOptions in
+// riegeli format.
+absl::StatusOr<std::string>
+AOTInterceptionPjrtClient::LoadSerializedArtifact() {
+  LOG(INFO) << "AOTInterceptionPjrtClient: Loading serialized executable "
+               "from: "
+            << artifact_path_;
+  std::string text_proto;
+  ABSL_RETURN_IF_ERROR(
+      tsl::ReadFileToString(tsl::Env::Default(), artifact_path_, &text_proto));
+
+  HumanReadableAotExecutable human_readable;
+  if (!tsl::protobuf::TextFormat::ParseFromString(text_proto,
+                                                  &human_readable)) {
+    return absl::InternalError(absl::StrCat(
+        "Failed to parse HumanReadableAotExecutable from ", artifact_path_));
+  }
+
+  LOG(INFO) << "AOTInterceptionPjrtClient: Parsed HumanReadableAotExecutable.";
+
+  ExecutableAndOptionsProto executable_and_options =
+      std::move(*human_readable.mutable_executable_and_options());
+
+  std::string serialized_gpu_exec;
+  ABSL_RETURN_IF_ERROR(WriteSplitGpuExecutable(
+      human_readable.gpu_executable(),
+      std::make_unique<riegeli::StringWriter<>>(&serialized_gpu_exec)));
+  *executable_and_options.mutable_serialized_executable() =
+      std::move(serialized_gpu_exec);
+
+  std::string serialized;
+  ABSL_RETURN_IF_ERROR(WriteSplitExecutableAndOptions(
+      executable_and_options,
+      std::make_unique<riegeli::StringWriter<>>(&serialized)));
+  LOG(INFO) << "AOTInterceptionPjrtClient: Successfully packed AOT artifact "
+               "into ExecutableAndOptions ("
+            << serialized.size() << " bytes).";
+  return serialized;
+}
 
 absl::StatusOr<std::unique_ptr<PjRtExecutable>>
 AOTInterceptionPjrtClient::Compile(const XlaComputation& computation,
                                    CompileOptions options) {
-  // Skeleton implementation: directly delegate to the underlying client.
+  if (mode_ == AOTTestMode::kBackwardsCompatibility) {
+    LOG(INFO) << "AOTInterceptionPjrtClient: Intercepting Compile in "
+                 "kBackwardsCompatibility mode for computation ["
+              << computation.name()
+              << "]. Deserializing executable instead of recompiling.";
+    ABSL_ASSIGN_OR_RETURN(std::string serialized, LoadSerializedArtifact());
+    LOG(INFO) << "AOTInterceptionPjrtClient: Calling "
+                 "inner_client_->DeserializeExecutable.";
+    auto exec_or =
+        inner_client_->DeserializeExecutable(serialized, std::move(options));
+    if (exec_or.ok()) {
+      LOG(INFO) << "AOTInterceptionPjrtClient: Successfully deserialized "
+                   "executable for ["
+                << computation.name() << "]";
+    } else {
+      LOG(ERROR) << "AOTInterceptionPjrtClient: Failed to deserialize "
+                    "executable for ["
+                 << computation.name() << "]: " << exec_or.status();
+    }
+    return exec_or;
+  }
+  LOG(INFO) << "AOTInterceptionPjrtClient: Compile called in "
+               "kGoldenVerification mode for computation ["
+            << computation.name() << "]. Delegating to underlying client.";
   return inner_client_->Compile(computation, std::move(options));
 }
 
 absl::StatusOr<std::unique_ptr<PjRtLoadedExecutable>>
 AOTInterceptionPjrtClient::CompileAndLoad(const XlaComputation& computation,
                                           CompileOptions options) {
-  // Skeleton implementation: directly delegate to the underlying client.
+  if (mode_ == AOTTestMode::kBackwardsCompatibility) {
+    LOG(INFO) << "AOTInterceptionPjrtClient: Intercepting CompileAndLoad in "
+                 "kBackwardsCompatibility mode for computation ["
+              << computation.name()
+              << "]. Deserializing and loading executable instead of "
+                 "recompiling.";
+    ABSL_ASSIGN_OR_RETURN(std::string serialized, LoadSerializedArtifact());
+    LOG(INFO) << "AOTInterceptionPjrtClient: Calling "
+                 "inner_client_->LoadSerializedExecutable.";
+    auto loaded_or = inner_client_->LoadSerializedExecutable(
+        serialized, std::move(options), LoadOptions());
+    if (loaded_or.ok()) {
+      LOG(INFO) << "AOTInterceptionPjrtClient: Successfully deserialized and "
+                   "loaded executable for ["
+                << computation.name() << "]";
+    } else {
+      LOG(ERROR) << "AOTInterceptionPjrtClient: Failed to load serialized "
+                    "executable for ["
+                 << computation.name() << "]: " << loaded_or.status();
+    }
+    return loaded_or;
+  }
+  LOG(INFO) << "AOTInterceptionPjrtClient: CompileAndLoad called in "
+               "kGoldenVerification mode for computation ["
+            << computation.name() << "]. Delegating to underlying client.";
   return inner_client_->CompileAndLoad(computation, std::move(options));
 }
 